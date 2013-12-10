32c767b9b34005800d6ce81d9eba091dc9c05735
Sorin <creyer@gmail.com>
2013-12-11 00:06:19 +0200

    make select consistent in where clause, added select in tests

--- phantom-dsl/src/test/scala/com/newzly/cassandra/phantom/dsl/CRUDTests.scala
+++ phantom-dsl/src/test/scala/com/newzly/cassandra/phantom/dsl/CRUDTests.scala
@@ -13,6 +13,83 @@ import com.datastax.driver.core.utils.UUIDs
 
 class CRUDTests extends BaseTest {
   implicit val session: Session = cassandraSession
+  "Select" should "work fine" in {
+    val primitivesTable =
+      """|CREATE TABLE primitives(
+        |key int PRIMARY KEY,
+        |long bigint,
+        |boolean boolean,
+        |bDecimal decimal,
+        |double double,
+        |float float,
+        |inet inet,
+        |int int,
+        |date timestamp,
+        |uuid uuid,
+        |bi varint);
+      """.stripMargin
+    cassandraSession.execute(primitivesTable)
+
+    case class Primitive(
+                          key: Int,
+                          long: Long,
+                          boolean: Boolean,
+                          bDecimal: BigDecimal,
+                          double: Double,
+                          float: Float,
+                          inet: java.net.InetAddress,
+                          int: Int,
+                          date: java.util.Date,
+                          uuid: java.util.UUID,
+                          bi: BigInt)
+
+    class Primitives extends CassandraTable[Primitives, Primitive] {
+      override def fromRow(r: Row): Primitive = {
+        Primitive(key(r), long(r), boolean(r), bDecimal(r), double(r), float(r), inet(r),
+          int(r), date(r), uuid(r), bi(r))
+      }
+      object key extends PrimitiveColumn[Int]
+      object long extends PrimitiveColumn[Long]
+      object boolean extends PrimitiveColumn[Boolean]
+      object bDecimal extends PrimitiveColumn[BigDecimal]
+      object double extends PrimitiveColumn[Double]
+      object float extends PrimitiveColumn[Float]
+      object inet extends PrimitiveColumn[java.net.InetAddress]
+      object int extends PrimitiveColumn[Int]
+      object date extends PrimitiveColumn[java.util.Date]
+      object uuid extends PrimitiveColumn[java.util.UUID]
+      object bi extends PrimitiveColumn[BigInt]
+
+    }
+    object Primitives extends Primitives {
+      override def tableName = "Primitives"
+    }
+
+    val row = Primitive(1, 2.toLong, true, BigDecimal("1.1"), 3.toDouble, 4.toFloat,
+      InetAddress.getByName("127.0.0.1"), 9, new java.util.Date, com.datastax.driver.core.utils.UUIDs.timeBased(),
+      BigInt(1002))
+    val rcp = Primitives.insert
+      .value(_.key, row.key)
+      .value(_.long, row.long)
+      .value(_.boolean, row.boolean)
+      .value(_.bDecimal, row.bDecimal)
+      .value(_.double, row.double)
+      .value(_.float, row.float)
+      .value(_.inet, row.inet)
+      .value(_.int, row.int)
+      .value(_.date, row.date)
+      .value(_.uuid, row.uuid)
+      .value(_.bi, row.bi)
+    rcp.execute().sync()
+
+    val recipeF: Future[Option[Primitive]] = Primitives.select.one
+    assert(recipeF.sync().get === row)
+    assert(Primitives.select.fetch.sync() contains (row))
+
+    val select1 = Primitives.select.where(_.key,1,EQ[Primitives,Int])
+    val s1 = select1.one.sync()
+    assert(s1.get === row)
+  }
 
   "Delete" should "work fine, when deleting the whole row" in {
     val primitivesTable =
