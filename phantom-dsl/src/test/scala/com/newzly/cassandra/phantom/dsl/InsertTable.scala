568716a300aed7e763a475e797e59a3c8387c2c0
Flavian Alexander <alexandruflavian23@gmail.com>
2013-12-10 00:04:21 +0000

    Syncing table names with Cassandra schema agreements.

--- phantom-dsl/src/test/scala/com/newzly/cassandra/phantom/dsl/InsertTable.scala
+++ phantom-dsl/src/test/scala/com/newzly/cassandra/phantom/dsl/InsertTable.scala
@@ -16,11 +16,6 @@ import com.newzly.cassandra.phantom.CassandraTable
 
 class InsertTable extends BaseTest {
 
-  implicit class SyncFuture[T](future: Future[T]) {
-    def sync(): T = {
-      Await.result(future, Duration(10, "seconds"))
-    }
-  }
 
   implicit val session: Session = cassandraSession
 
@@ -28,7 +23,7 @@ class InsertTable extends BaseTest {
     //char is not supported
     //https://github.com/datastax/java-driver/blob/2.0/driver-core/src/main/java/com/datastax/driver/core/DataType.java
     val primitivesTable =
-      """|CREATE TABLE primitives(
+      """|CREATE TABLE Primitives(
         |str text PRIMARY KEY,
         |long bigint,
         |boolean boolean,
@@ -43,7 +38,7 @@ class InsertTable extends BaseTest {
       """.stripMargin
     cassandraSession.execute(primitivesTable)
 
-    case class Primitives(
+    case class Primitive(
       str: String,
       long: Long,
       boolean: Boolean,
@@ -56,9 +51,9 @@ class InsertTable extends BaseTest {
       uuid: java.util.UUID,
       bi: BigInt)
 
-    class PrimitivesTable extends CassandraTable[PrimitivesTable, Primitives] {
-      override def fromRow(r: Row): Primitives = {
-        Primitives(str(r), long(r), boolean(r), bDecimal(r), double(r), float(r), inet(r),
+    class Primitives extends CassandraTable[Primitives, Primitive] {
+      override def fromRow(r: Row): Primitive = {
+        Primitive(str(r), long(r), boolean(r), bDecimal(r), double(r), float(r), inet(r),
           int(r), date(r), uuid(r), bi(r))
       }
 
@@ -75,13 +70,13 @@ class InsertTable extends BaseTest {
       object uuid extends PrimitiveColumn[UUID]
       object bi extends PrimitiveColumn[BigInt]
     }
-    object PrimitivesTable extends PrimitivesTable
+    object Primitives extends Primitives
 
-    val row = Primitives("myString", 2.toLong, true, BigDecimal("1.1"), 3.toDouble, 4.toFloat,
+    val row = Primitive("myString", 2.toLong, true, BigDecimal("1.1"), 3.toDouble, 4.toFloat,
       InetAddress.getByName("127.0.0.1"), 9, new java.util.Date, com.datastax.driver.core.utils.UUIDs.timeBased(),
       BigInt(1002
       ))
-    val rcp = PrimitivesTable.insert
+    val rcp = Primitives.insert
       .value(_.str, row.str)
       .value(_.long, row.long)
       .value(_.boolean, row.boolean)
@@ -94,15 +89,15 @@ class InsertTable extends BaseTest {
       .value(_.uuid, row.uuid)
       .value(_.bi, row.bi)
     rcp.execute().sync()
-    val recipeF: Future[Option[Primitives]] = PrimitivesTable.select.one
+    val recipeF: Future[Option[Primitive]] = Primitives.select.one
     assert(recipeF.sync().get === row)
 
-    assert(PrimitivesTable.select.fetch.sync() contains (row))
+    assert(Primitives.select.fetch.sync() contains (row))
   }
 
   it should "work fine with List, Set, Map" in {
     val createTestTable =
-      """|CREATE TABLE testTable(
+      """|CREATE TABLE TestTable(
         |key text PRIMARY KEY,
         |list list<text>,
         |setText set<text>,
@@ -140,6 +135,7 @@ class InsertTable extends BaseTest {
     val row = TestRow("w", Seq("ee", "pp", "ee3"), Set("u", "e"), Map("k" -> "val"), Set(1, 22, 2),
       Map(3 -> "OO"))
     object TestTable extends TestTable
+
     val rcp = TestTable.insert
       .value(_.key, row.key)
       .value(_.list, row.list)
@@ -157,7 +153,7 @@ class InsertTable extends BaseTest {
 
   it should "work fine with custom types" in {
     val createTestTable =
-      """|CREATE TABLE myTest(
+      """|CREATE TABLE MyTest(
         |key text PRIMARY KEY,
         |optionA int,
         |classS text,
@@ -168,7 +164,7 @@ class InsertTable extends BaseTest {
     case class ClassS(something: String)
     case class TestRow(key: String, optionA: Option[Int], classS: ClassS)
 
-    class TestTable extends CassandraTable[TestTable, TestRow] {
+    class MyTest extends CassandraTable[MyTest, TestRow] {
       def fromRow(r: Row): TestRow = {
         TestRow(key(r), optionA(r), classS(r))
       }
@@ -178,21 +174,22 @@ class InsertTable extends BaseTest {
     }
 
     val row = TestRow("someKey", Some(2), ClassS("lol"))
-    object TestTable extends TestTable
-    val rcp = TestTable.insert
+    object MyTest extends MyTest
+
+    val rcp = MyTest.insert
       .value(_.key, row.key)
       .valueOrNull(_.optionA, row.optionA)
       .value(_.classS, row.classS)
     rcp.execute().sync()
-    val recipeF: Future[Option[TestRow]] = TestTable.select.one
+    val recipeF: Future[Option[TestRow]] = MyTest.select.one
     assert(recipeF.sync().get === row)
 
-    assert(TestTable.select.fetch.sync() contains (row))
+    assert(MyTest.select.fetch.sync() contains (row))
   }
 
   it should "work fine with Mix" in {
     val recipesTable =
-      """|CREATE TABLE recipes (
+      """|CREATE TABLE Recipes (
         |url text PRIMARY KEY,
         |description text,
         |ingredients list<text>,
@@ -257,7 +254,7 @@ class InsertTable extends BaseTest {
 
   ignore should "work here but it fails- WE NEED TO FIX IT" in {
     val createTestTable =
-      """|CREATE TABLE myTest(
+      """|CREATE TABLE TestTable2(
         |key text PRIMARY KEY,
         |optionA int,
         |classS text,
@@ -270,7 +267,7 @@ class InsertTable extends BaseTest {
     case class ClassS(something: Map[String, Int])
     case class TestRow(key: String, optionA: Option[Int], classS: ClassS, optionS: Option[ClassS], map: Map[String, ClassS])
 
-    class TestTable extends CassandraTable[TestTable, TestRow] {
+    class TestTable2 extends CassandraTable[TestTable2, TestRow] {
       def fromRow(r: Row): TestRow = {
         TestRow(key(r), optionA(r), classS(r), optionS(r), mapIntoClass(r))
       }
@@ -282,21 +279,27 @@ class InsertTable extends BaseTest {
     }
 
     val row = TestRow("someKey", Some(2), ClassS(Map("k2" -> 5)), Some(ClassS(Map("k2" -> 5))), Map("5" -> ClassS(Map("p" -> 2))))
-    object TestTable extends TestTable
-    val rcp = TestTable.insert
+    object TestTable2 extends TestTable2
+
+    val rcp = TestTable2.insert
       .value(_.key, row.key)
       .valueOrNull(_.optionA, row.optionA)
       .value(_.classS, row.classS)
       .value(_.optionS, row.optionS)
       .value(_.mapIntoClass, row.map)
+
+
     rcp.qb.enableTracing()
+
     info(rcp.toString)
     info(rcp.qb.toString)
+
     rcp.execute().sync()
-    val recipeF: Future[Option[TestRow]] = TestTable.select.one
+
+    val recipeF: Future[Option[TestRow]] = TestTable2.select.one
 
     assert(recipeF.sync().get === row)
-    assert(TestTable.select.fetch.sync() contains (row))
+    assert(TestTable2.select.fetch.sync() contains (row))
   }
 
 }
