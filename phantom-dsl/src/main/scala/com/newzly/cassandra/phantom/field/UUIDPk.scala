657d5ef173b4825d63de3c4eafb66b3c6301f964
Flavian Alexander <alexandruflavian23@gmail.com>
2013-12-15 00:49:54 +0000

    Removing concrete object type from Pk mixins because it's useless.

--- phantom-dsl/src/main/scala/com/newzly/cassandra/phantom/field/UUIDPk.scala
+++ phantom-dsl/src/main/scala/com/newzly/cassandra/phantom/field/UUIDPk.scala
@@ -3,24 +3,22 @@ package com.newzly.cassandra.phantom.field
 import java.util.UUID
 import com.newzly.cassandra.phantom.{ CassandraTable, PrimitiveColumn }
 
-trait UUIDPk[Owner <: CassandraTable[Owner, Record], Record] {
-  this: CassandraTable[Owner, Record] =>
+trait UUIDPk[Owner <: CassandraTable[Owner, _]] {
+  this: CassandraTable[Owner, _] =>
 
   object id extends PrimitiveColumn[UUID] {}
 
   val _key = id;
 }
 
-trait TimeUUIDPk[Owner <: CassandraTable[Owner, Record], Record] {
-  this: CassandraTable[Owner, Record] =>
+trait TimeUUIDPk[Owner <: CassandraTable[Owner, _]] extends  UUIDPk[Owner] {
+  this: CassandraTable[Owner, _] =>
 
-  object id extends PrimitiveColumn[UUID] {
-
-  }
-  val _key = id;
+  object id extends PrimitiveColumn[UUID]
+  override val _key = id;
 }
 
-trait LongOrderKey[Owner <: CassandraTable[Owner, Record], Record] {
-  this: CassandraTable[Owner, Record] with UUIDPk[Owner, Record] =>
+trait LongOrderKey[Owner <: CassandraTable[Owner, _]] {
+  this: CassandraTable[Owner, _] with UUIDPk[Owner] =>
   object order_id extends PrimitiveColumn[Long]
 }
---
+++ phantom-dsl/src/test/scala/com/newzly/cassandra/phantom/dsl/SkippingRecordsTest.scala
@@ -0,0 +1,31 @@
+package com.newzly.cassandra.phantom.dsl
+
+import java.util.UUID
+
+import org.scalatest.concurrent.ScalaFutures
+
+import com.datastax.driver.core.Row
+import com.newzly.cassandra.phantom.{PrimitiveColumn, CassandraTable}
+import com.newzly.cassandra.phantom.field.{LongOrderKey, TimeUUIDPk}
+
+class SkippingRecordsTest extends BaseTest with ScalaFutures {
+
+  it should "allow skipping records " in {
+
+    case class Article(val name: String, id: UUID, order_id: Long)
+    class Articles extends CassandraTable[Articles, Article] with TimeUUIDPk[Articles] with LongOrderKey[Articles] {
+
+      object name extends PrimitiveColumn[String]
+
+      override def fromRow(row: Row): Article = {
+        Article(name(row), id(row), order_id(row))
+      }
+    }
+
+    object Articles extends Articles {
+      override val tableName = "articles"
+    }
+
+  }
+
+}
