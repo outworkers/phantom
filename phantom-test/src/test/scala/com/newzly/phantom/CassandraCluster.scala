49ef56d0330372fe59523fb28c9df3d86df64863
Flavian Alexander <alexandruflavian23@gmail.com>
2013-12-15 14:01:13 +0000

    Adding copyright to all new files.

--- phantom-test/src/test/scala/com/newzly/phantom/CassandraCluster.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/CassandraCluster.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 newzly ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.newzly.phantom
 
 import com.datastax.driver.core.Cluster
@@ -6,24 +21,6 @@ import org.apache.cassandra.io.util.FileUtils
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 object CassandraInst {
 
-  //lazy
-  /*val service1 = {
-    try {
-    System.setProperty("cassandra.config", getClass.getClassLoader.getResource("cassandra.yaml").toString)
-    System.setProperty("cassandra-foreground", "yes")
-    System.setProperty("log4j.defaultInitOverride", "false")
-    val f = new java.io.File("tmp")
-    if (f.exists()) FileUtils.deleteRecursive(f)
-    f.mkdir()
-    println (f.getAbsolutePath)
-    val s = new EmbeddedCassandraService
-    s.start()
-   // s
-    } catch {
-      case e: Exception =>
-        Nil
-    }
-  }*/
   val s = {
     println("======== start embedding cassandra server ===========")
     val f = new java.io.File("tmp")
