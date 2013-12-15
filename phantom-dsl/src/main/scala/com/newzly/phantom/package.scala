90587d7b8f2637c1e4bc30ed40679dbc6e40b349
Flavian Alexander <alexandruflavian23@gmail.com>
2013-12-15 13:54:37 +0000

    Removed Lift record dependency. Using only Lift JSON, Lift integration no longer in plan. Separating tests into a new module.

--- phantom-dsl/src/main/scala/com/newzly/phantom/package.scala
+++ phantom-dsl/src/main/scala/com/newzly/phantom/package.scala
@@ -1,7 +1,15 @@
 package com.newzly.cassandra
 
 import com.newzly.phantom.Column
+import com.datastax.driver.core.Row
 
 package object phantom {
+
+  type UUID = java.util.UUID
+  type Date = java.util.Date
+
   type DateTime = org.joda.time.DateTime
+
+
+  type Row = com.datastax.driver.core.Row
 }
