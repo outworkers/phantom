b3667c113e832fd5b5898149b29a706ead9d014c
Flavian Alexander <alexandruflavian23@gmail.com>
2014-01-09 16:22:48 +0000

    Adding an async assertions helper from the GDN project. Testing deletion with async assertions.

---
+++ phantom-test/src/test/scala/com/newzly/phantom/helper/AsyncAssertionsHelper.scala
@@ -0,0 +1,40 @@
+package com.newzly.phantom.helper
+
+import org.scalatest.Assertions
+import org.scalatest.concurrent.AsyncAssertions
+import com.twitter.util.Future
+
+object AsyncAssertionsHelper {
+  implicit class Failing[A](val f: Future[A]) extends Assertions with AsyncAssertions {
+    def failing[T  <: Throwable : Manifest] = {
+      val w = new Waiter
+
+      f onSuccess  {
+        res => w.dismiss()
+      }
+
+      f onFailure {
+        e => w(throw e); w.dismiss()
+      }
+      intercept[T] {
+        w.await
+      }
+    }
+
+    def successful(x: A => Unit) = {
+      val w = new Waiter
+
+      f onSuccess {
+        case res => x(res); w.dismiss()
+      }
+
+      f onFailure {
+        e => w(throw e); w.dismiss()
+      }
+
+      w.await()
+    }
+  }
+
+
+}
