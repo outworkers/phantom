6dd4cd96b2ca2602ee989cba333270676a0c53d6
Flavian Alexander <alexandruflavian23@gmail.com>
2014-01-22 12:35:12 +0000

    Adding TTL support for insert queries.

---
+++ phantom-test/src/test/scala/com/newzly/phantom/dsl/crud/TTLTest.scala
@@ -0,0 +1,10 @@
+package com.newzly.phantom.dsl.crud
+
+import org.scalatest.{ Assertions, Matchers }
+import org.scalatest.concurrent.AsyncAssertions
+import com.newzly.phantom.dsl.BaseTest
+import com.newzly.phantom.helper.Tables
+
+class TTLTest extends BaseTest with Matchers with Tables with Assertions with AsyncAssertions {
+
+}
