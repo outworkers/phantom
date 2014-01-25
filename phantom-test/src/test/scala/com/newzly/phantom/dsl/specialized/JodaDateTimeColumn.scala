52156cb4a389e874c7200c5c0a7e43eff349835a
Flavian Alexander <alexandruflavian23@gmail.com>
2014-01-25 19:33:17 +0000

    Fixing Joda column test

--- phantom-test/src/test/scala/com/newzly/phantom/dsl/specialized/JodaDateTimeColumn.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/dsl/specialized/JodaDateTimeColumn.scala
@@ -4,43 +4,25 @@ import org.joda.time.DateTime
 import org.scalatest.{ Assertions, Matchers }
 import org.scalatest.concurrent.{ AsyncAssertions, PatienceConfiguration }
 import org.scalatest.time.SpanSugar._
-import com.datastax.driver.core.Row
-import com.newzly.phantom.{ CassandraTable, PrimitiveColumn }
 import com.newzly.phantom.helper.AsyncAssertionsHelper._
 import com.newzly.phantom.helper.BaseTest
+import com.newzly.phantom.tables.{ JodaRow, PrimitivesJoda }
 
 class JodaDateTimeColumn extends BaseTest with Matchers with Assertions with AsyncAssertions {
   val keySpace: String = "UpdateTest"
   implicit val s: PatienceConfiguration.Timeout = timeout(10 seconds)
 
   it should "work fine" in {
-    case class JodaRow( pkey: String, int: Int,
-                          bi: DateTime)
-    class PrimitivesJoda extends CassandraTable[PrimitivesJoda, JodaRow] {
-      override def fromRow(r: Row): JodaRow = {
-        JodaRow(pkey(r),int(r),  bi(r))
-      }
-      object pkey extends PrimitiveColumn[String]
-      object int extends PrimitiveColumn[Int]
-      object bi extends PrimitiveColumn[DateTime]
-      val _key = pkey
-    }
-
-    object PrimitivesJoda extends PrimitivesJoda {
-      override def tableName = "PrimitivesJoda"
-    }
-
-    val dt = DateTime.now()
-    val row =  JodaRow("w",1,dt)
+    val row = JodaRow.sample
 
     val w = PrimitivesJoda.create(_.pkey,_.int,_.bi).execute() flatMap {
       _ => {
         PrimitivesJoda.insert.value(_.pkey,"w").value(_.int,1)
-          .value(_.bi,dt).execute()
+          .value(_.bi, row.bi).execute()
         }
       } flatMap  {
-          _ => PrimitivesJoda.select.one
-        }
+        _ => PrimitivesJoda.select.one
+      }
 
     w successful {
       case res => assert(res.get === row)
---
+++ phantom-test/src/test/scala/com/newzly/phantom/tables/PrimitivesJoda.scala
@@ -0,0 +1,38 @@
+package com.newzly.phantom.tables
+
+import org.joda.time.DateTime
+import com.newzly.phantom.{PrimitiveColumn, CassandraTable}
+import com.datastax.driver.core.Row
+import com.newzly.phantom.helper.{TestSampler, Sampler, ModelSampler}
+
+
+case class JodaRow( pkey: String, int: Int,
+  bi: DateTime)
+
+object JodaRow extends ModelSampler {
+  def sample: JodaRow = {
+    JodaRow(
+      Sampler.getAUniqueString,
+      Sampler.getARandomInteger(),
+      new DateTime()
+    )
+  }
+}
+
+sealed class PrimitivesJoda extends CassandraTable[PrimitivesJoda, JodaRow] {
+  override def fromRow(r: Row): JodaRow = {
+    JodaRow(pkey(r),int(r),  bi(r))
+  }
+  object pkey extends PrimitiveColumn[String]
+  object int extends PrimitiveColumn[Int]
+  object bi extends PrimitiveColumn[DateTime]
+  val _key = pkey
+}
+
+object PrimitivesJoda extends PrimitivesJoda with TestSampler {
+
+  override val tableName = "PrimitivesJoda"
+
+  def createSchema = ""
+}
+
