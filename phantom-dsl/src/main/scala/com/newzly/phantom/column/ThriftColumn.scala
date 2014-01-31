91dd298c068712fd617cb7c0b8ed30f83d56ccaa
Flavian Alexander <alexandruflavian23@gmail.com>
2014-01-31 12:55:38 +0000

    Great success. Thrift serialization works.

--- phantom-dsl/src/main/scala/com/newzly/phantom/column/ThriftColumn.scala
+++ phantom-dsl/src/main/scala/com/newzly/phantom/column/ThriftColumn.scala
@@ -1,29 +1,23 @@
 package com.newzly.phantom.column
 
-import java.nio.charset.Charset
-import org.apache.thrift.protocol.{TCompactProtocol, TBinaryProtocol}
-import org.apache.thrift.transport.TMemoryInputTransport
 import com.datastax.driver.core.Row
 import com.newzly.phantom.CassandraTable
-import com.twitter.scrooge.{ ThriftStruct, ThriftStructCodec3 }
+import com.twitter.scrooge.{ CompactThriftSerializer, ThriftStruct}
 import com.twitter.util.Try
 
 abstract class ThriftColumn[Owner <: CassandraTable[Owner, Record], Record, ValueType <: ThriftStruct](table: CassandraTable[Owner, Record]) extends Column[Owner, Record, ValueType](table) {
 
   def toCType(v: ValueType): AnyRef = {
-    table.logger.info(s"Serialized to Thrift Binary: ${v.toString}")
-    v.toString
+    serializer.toString(v)
   }
 
-  def encode(v: ValueType): Array[Byte]
-  def decode(data: Array[Byte]): ValueType
+  def serializer: CompactThriftSerializer[ValueType]
 
   val cassandraType = "text"
 
   def optional(r: Row): Option[ValueType] = {
     Try {
-      val data = r.getString(name)
-      Some(decode(data.getBytes(Charset.forName("UTF-8"))))
+      Some(serializer.fromString(r.getString(name)))
     } getOrElse None
   }
 }
--- phantom-test/src/test/scala/com/newzly/phantom/tables/ThriftColumnTable.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/tables/ThriftColumnTable.scala
@@ -1,6 +1,6 @@
 package com.newzly.phantom.tables
 
-import org.apache.thrift.protocol.TBinaryProtocol
+import org.apache.thrift.protocol.{TCompactProtocol, TBinaryProtocol}
 import org.apache.thrift.transport.TMemoryInputTransport
 
 import com.datastax.driver.core.Row
@@ -9,7 +9,7 @@ import com.newzly.phantom.Implicits._
 import com.newzly.phantom.keys.PrimaryKey
 import com.newzly.phantom.helper.TestSampler
 import com.newzly.phantom.thrift.ThriftTest
-import com.thinkaurelius.thrift.util
+import com.twitter.scrooge.CompactThriftSerializer
 
 case class Output(id: Int, name: String, struct: ThriftTest)
 
@@ -20,17 +20,8 @@ sealed class ThriftColumnTable extends CassandraTable[ThriftColumnTable, Output]
   object id extends IntColumn(this) with PrimaryKey
   object name extends StringColumn(this)
   object ref extends ThriftColumn[ThriftColumnTable, Output, ThriftTest](this) {
-
-    def encode(item: ThriftTest): Array[Byte] = {
-      val trans = new TMemoryInputTransport()
-      val protocol = new TBinaryProtocol(trans)
-      ThriftTest.encode(item.copy(), protocol)
-      item.toString.getBytes
-    }
-
-    def decode(data: Array[Byte]): ThriftTest = {
-      val _trans = new TMemoryInputTransport(data)
-      ThriftTest.decode(new TBinaryProtocol(_trans))
+    val serializer = new CompactThriftSerializer[ThriftTest] {
+      def codec = ThriftTest
     }
   }
 
