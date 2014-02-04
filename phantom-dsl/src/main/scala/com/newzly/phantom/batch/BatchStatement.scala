733e75b8f5b6629e55b1e04a2f2d29c6d2d5bf6c
Sorin <creyer@gmail.com>
2014-02-05 01:37:09 +0200

    batch statement without individual timestamp support

--- phantom-dsl/src/main/scala/com/newzly/phantom/batch/BatchStatement.scala
+++ phantom-dsl/src/main/scala/com/newzly/phantom/batch/BatchStatement.scala
@@ -4,25 +4,37 @@ import com.newzly.phantom.query.{ ExecutableStatement }
 import com.datastax.driver.core.{BatchStatement => DatastaxBatchStatement, ResultSet, Session}
 import com.newzly.phantom.CassandraResultSetOperations
 import com.twitter.util.Future
-import scala.collection.parallel.mutable.ParHashSet
 import scala.collection.mutable.ListBuffer
 
 sealed trait BatchQueryListTrait extends CassandraResultSetOperations{
   protected[this] lazy val statements: ListBuffer[ExecutableStatement] =  ListBuffer.empty[ExecutableStatement]
-  def add(statement: ExecutableStatement)
+  def add(statement: ExecutableStatement): BatchStatement
   def execute()(implicit session: Session) :Future[ResultSet]
+  def qbList: Seq[ExecutableStatement]
 }
 
-class BatchStatement extends BatchQueryListTrait {
+/**
+ * !!! Attention !!!
+ * This class is not meant to be used for concurrent operations on the same row inside one batch.
+ * In order to have concurrent operation on the same row in the same batch, custom timesatmps needs to be inserted
+ * on each statement. This is not in the scope of this class.(for now)
+ */
+class BatchStatement extends BatchQueryListTrait{
+   val qbList = Seq.empty[ExecutableStatement]
 
-  def add(statement: ExecutableStatement): Unit = {
-    statements += statement
+  def apply(list: Seq[ExecutableStatement] = Seq.empty[ExecutableStatement]) = {
+    new BatchStatement(){
+      override val qbList = list
+    }
+  }
+
+  def add(statement: ExecutableStatement): BatchStatement = {
+     apply(qbList :+ statement)
   }
 
   def execute()(implicit session: Session):Future[ResultSet] = {
     val batch = new DatastaxBatchStatement()
-    for (s <- statements) {
-      Console.println(s.qb)
+    for (s <- qbList) {
       batch.add(s.qb)
     }
     statementExecuteToFuture(batch)
