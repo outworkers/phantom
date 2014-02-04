2b7294cb6493ad208a9daa59e0e6a678aee128b9
Sorin <creyer@gmail.com>
2014-02-05 01:37:09 +0200

    batch statement without individual timestamp support

--- phantom-test/src/test/scala/com/newzly/phantom/dsl/batch/BatchTest.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/dsl/batch/BatchTest.scala
@@ -7,6 +7,7 @@ import com.newzly.phantom.tables.{PrimitivesJoda, JodaRow}
 import org.scalatest.time.SpanSugar._
 import com.newzly.phantom.helper.AsyncAssertionsHelper._
 import com.newzly.phantom.batch.BatchStatement
+import org.joda.time.DateTime
 
 class BatchTest extends BaseTest with Matchers with Assertions with AsyncAssertions {
   val keySpace: String = "BatchTestSpace"
@@ -14,32 +15,30 @@ class BatchTest extends BaseTest with Matchers with Assertions with AsyncAsserti
 
   it should "work fine" in {
     val row = JodaRow.sample
-    val row2 = row.copy(int = JodaRow.sample.int, bi = JodaRow.sample.bi)
+    val row2 = JodaRow.sample.copy(pkey = row.pkey)
     val row3 = JodaRow.sample
     PrimitivesJoda.insertSchema(session)
     val statement1 = PrimitivesJoda.insert
         .value(_.pkey, row.pkey)
         .value(_.intColumn, row.int)
         .value(_.timestamp, row.bi)
-    val statement2 = PrimitivesJoda.update
-      .where(_.pkey eqs row2.pkey)
-      .modify(_.intColumn, row2.int)
-      .modify(_.timestamp, row2.bi)
-    val statement3 = PrimitivesJoda.insert
+    val statement2 = PrimitivesJoda.insert
       .value(_.pkey, row3.pkey)
       .value(_.intColumn, row3.int)
       .value(_.timestamp, row3.bi)
+    val statement3 = PrimitivesJoda.update
+      .where(_.pkey eqs row2.pkey)
+      .modify(_.intColumn, row2.int)
+      .modify(_.timestamp, row2.bi)
     val statement4 = PrimitivesJoda.delete
       .where(_.pkey eqs row3.pkey)
-    val batch = new BatchStatement()
 
-    batch.add(statement1)
-    batch.add(statement2)
-    batch.add(statement3)
-    batch.add(statement4)
-    batch.execute().sync()
-    Thread.sleep(1000)
+    val batch = new BatchStatement().add(statement3).add(statement4)
+
     val w = for {
+      s1 <- statement1.execute()
+      s3 <- statement2.execute()
+      b <- batch.execute()
       updated <- PrimitivesJoda.select.where(_.pkey eqs row.pkey).execute()
       deleted <- PrimitivesJoda.select.where(_.pkey eqs row3.pkey).execute()
     } yield (updated, deleted)
