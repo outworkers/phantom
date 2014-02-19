3667ef43172c256ae85f5666d724b11964087351
Flavian Alexander <alexandruflavian23@gmail.com>
2014-02-19 00:31:23 +0000

    Adding basic examples

---
+++ phantom-example/src/main/scala/com/newzly/phantom/example/ThriftModels.scala
@@ -0,0 +1,30 @@
+package com.newzly.phantom.example
+
+import java.util.UUID
+import com.datastax.driver.core.Row
+import com.newzly.phantom.Implicits._
+import com.newzly.phantom.thrift.ThriftColumn
+import com.twitter.scrooge.{ThriftStructCodec, CompactThriftSerializer}
+
+
+case class SampleRecord(
+  stuff: String,
+  someList: List[String],
+  thriftModel: SampleModel
+)
+
+sealed class ThriftTable extends CassandraTable[ThriftTable,  SampleRecord] {
+  object id extends UUIDColumn(this) with PartitionKey[UUID]
+  object stuff extends StringColumn(this)
+  object someList extends ListColumn[ThriftTable, SampleRecord, String](this)
+  object thriftModel extends ThriftColumn[ThriftTable, SampleRecord, SampleModel](this) {
+    def serializer = new CompactThriftSerializer[SampleModel] {
+      override def codec = SampleModel
+    }
+  }
+
+
+  def fromRow(r: Row): SampleRecord = {
+    SampleRecord(stuff(r), someList(r), thriftModel(r))
+  }
+}
