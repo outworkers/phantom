004619dc3268c86c83cd5d4e27607da493dbf902
Flavian Alexander <alexandruflavian23@gmail.com>
2014-02-20 17:19:33 +0000

    Fixing seq and set confusion issue for Thrift records.

--- phantom-finagle/src/main/scala/com/newzly/phantom/finagle/Implicits.scala
+++ phantom-finagle/src/main/scala/com/newzly/phantom/finagle/Implicits.scala
@@ -1,18 +1,10 @@
 package com.newzly.phantom.finagle
 
-import scala.collection.JavaConverters._
-import com.datastax.driver.core.{ BatchStatement => DatastaxBatchStatement, ResultSet, Session, Statement}
-import com.google.common.util.concurrent.{Futures, FutureCallback}
-import com.newzly.phantom.{CassandraTable, CassandraResultSetOperations, Manager}
-import com.newzly.phantom.batch.BatchStatement
-import com.newzly.phantom.query._
-import com.twitter.util.{Duration, Future, Promise}
-import com.newzly.phantom.iteratee.{Iteratee, Enumerator}
-import play.api.libs.iteratee.{Enumerator => PlayEnumerator, Iteratee => PlayIteratee, Enumeratee}
-import scala.concurrent.{Future => ScalaFuture, ExecutionContext}
+import scala.concurrent.{ ExecutionContext, Future => ScalaFuture }
+import com.twitter.util.{ Future, Promise }
 
 object Implicits {
-  implicit def transformToTwitterFuture[R](sf: ScalaFuture[R])(implicit executorContext: ExecutionContext = Manager.scalaExecutor): Future[R] = {
+  implicit def transformToTwitterFuture[R](sf: ScalaFuture[R])(implicit executorContext: ExecutionContext): Future[R] = {
       val p = new Promise[R]
 
       sf.onSuccess {
