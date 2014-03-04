f59c231fd02c2f1ecaa34a5a52c084cdca5d83b2
Flavian Alexandru <flaviana@blinkbox.com>
2014-03-04 16:30:46 +0000

    Fixed prepend test.

--- phantom-test/src/test/scala/com/newzly/phantom/dsl/crud/ListOperatorsTest.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/dsl/crud/ListOperatorsTest.scala
@@ -77,4 +77,67 @@ class ListOperatorsTest extends BaseTest {
     }
   }
 
+  it should "prepend an item to a list" in {
+    Recipes.insertSchema
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .future()
+
+    val operation = for {
+      insertDone <- insert
+      update <- Recipes.update.where(_.url eqs recipe.url).modify(_.ingredients prepend "test").future()
+      select <- Recipes.select(_.ingredients).where(_.url eqs recipe.url).one
+    } yield {
+      select
+    }
+
+    operation.successful {
+      items => {
+        Console.println(s"${items.mkString(" ")}")
+        items.isDefined shouldBe true
+        items.get shouldBe List("test") :::  recipe.ingredients
+      }
+    }
+  }
+
+  it should "prepend several items to a list" in {
+    Recipes.insertSchema
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .future()
+
+    val appendable = List("test", "test2")
+    val operation = for {
+      insertDone <- insert
+      update <- Recipes.update.where(_.url eqs recipe.url).modify(_.ingredients prependAll appendable).future()
+      select <- Recipes.select(_.ingredients).where(_.url eqs recipe.url).one
+    } yield {
+      select
+    }
+
+    operation.successful {
+      items => {
+        Console.println(s"${items.mkString(" ")}")
+        items.isDefined shouldBe true
+        items.get shouldBe appendable.reverse ::: recipe.ingredients
+      }
+    }
+  }
+
 }
