c365d107271f124bf102c20eee30ae72c12ec139
Flavian Alexandru <flaviana@blinkbox.com>
2014-04-16 13:58:34 +0100

    Adding examples.

---
+++ phantom-example/src/main/scala/com/newzly/phantom/example/DBConnector.scala
@@ -0,0 +1,31 @@
+package com.newzly.phantom.example
+
+
+import scala.concurrent. { blocking, Future }
+import com.datastax.driver.core.{ Cluster, Session }
+import com.newzly.phantom.Implicits._
+
+object DBConnector {
+  val keySpace = "phantom_examples"
+
+  lazy val cluster =  Cluster.builder()
+    .addContactPoint("localhost")
+    .withPort(9042)
+    .withoutJMXReporting()
+    .withoutMetrics()
+    .build()
+
+  lazy val session = blocking {
+    cluster.connect(keySpace)
+  }
+}
+
+trait DBConnector {
+  self: CassandraTable[_, _] =>
+
+  def createTable(): Future[Unit] ={
+    create.future() map (_ => ())
+  }
+
+  implicit lazy val datastax: Session = DBConnector.session
+}
