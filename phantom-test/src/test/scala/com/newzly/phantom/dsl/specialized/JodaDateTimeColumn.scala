d481c9a21b32e83c46921b8aa39414cd39a42807
Flavian Alexander <alexandruflavian23@gmail.com>
2014-04-23 13:05:26 +0100

    Feature/0.4.0: In operators, static columns, query restrictions, Twitter Futures now first class citizens.

--- phantom-test/src/test/scala/com/newzly/phantom/dsl/specialized/JodaDateTimeColumn.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/dsl/specialized/JodaDateTimeColumn.scala
@@ -2,7 +2,7 @@ package com.newzly.phantom.dsl.specialized
 
 import org.scalatest.concurrent.PatienceConfiguration
 import org.scalatest.time.SpanSugar._
-import com.newzly.util.finagle.AsyncAssertionsHelper._
+import com.newzly.util.testing.AsyncAssertionsHelper._
 import com.newzly.phantom.helper.BaseTest
 import com.newzly.phantom.tables.{ JodaRow, PrimitivesJoda }
 
--- phantom-test/src/test/scala/com/newzly/phantom/iteratee/IterateeTakeTest.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/iteratee/IterateeTakeTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.concurrent.PatienceConfiguration
 import org.scalatest.time.SpanSugar._
 import com.newzly.phantom.helper.BaseTest
 import com.newzly.phantom.tables.{ Primitive, Primitives }
-import com.newzly.util.finagle.AsyncAssertionsHelper._
+import com.newzly.util.testing.AsyncAssertionsHelper._
 
 class IterateeTakeTest extends BaseTest {
   val keySpace: String = "iteratee_take_tests"
