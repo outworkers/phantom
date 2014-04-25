23ef7f8a06882fdeb8e89c9245a2a82a5e0a4f36
Flavian Alexandru <flaviana@blinkbox.com>
2014-04-25 11:29:01 +0100

    More cleanup.

--- phantom-test/src/test/scala/com/newzly/phantom/iteratee/BigTest.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/iteratee/BigTest.scala
@@ -1,8 +1,8 @@
 package com.newzly.phantom.iteratee
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ blocking, ExecutionContext }
 import org.scalatest.{ Assertions, BeforeAndAfterAll, FlatSpec, Matchers  }
-import org.scalatest.concurrent.{AsyncAssertions, ScalaFutures}
+import org.scalatest.concurrent.{ AsyncAssertions, ScalaFutures }
 import com.datastax.driver.core.Session
 import com.newzly.phantom.Manager
 import com.twitter.util.Duration
@@ -12,7 +12,6 @@ trait BigTest extends FlatSpec with ScalaFutures with BeforeAndAfterAll with Mat
   val cluster = BigTestHelper.cluster
   implicit lazy val session: Session = cluster.connect()
   implicit lazy val context: ExecutionContext = Manager.scalaExecutor
-
 
   private[this] def createKeySpace(spaceName: String) = {
     session.execute(s"CREATE KEYSPACE $spaceName WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};")
@@ -26,7 +25,9 @@ trait BigTest extends FlatSpec with ScalaFutures with BeforeAndAfterAll with Mat
   }
 
   override def afterAll() {
-    session.execute(s"DROP KEYSPACE $keySpace;")
+    blocking {
+      session.execute(s"DROP KEYSPACE $keySpace;")
+    }
   }
 
 }
