516aa8304979c56ebb55fc85d3e1f35f8ce70ec8
Flavian Alexander <alexandruflavian23@gmail.com>
2014-05-09 01:17:14 +0100

    Moving test tables to the appropiate place.

--- phantom-test/src/test/scala/com/newzly/phantom/TableKeyGenerationTest.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/TableKeyGenerationTest.scala
@@ -1,64 +1,12 @@
 package com.newzly.phantom
 
-import java.util.UUID
 import org.scalatest.{ FlatSpec, Matchers, ParallelTestExecution }
-import com.datastax.driver.core.Row
-import com.newzly.phantom.Implicits._
-
-case class StubRecord(name: String, id: UUID)
-sealed class TableWithSingleKey extends CassandraTable[TableWithSingleKey, StubRecord] {
-
-  object id extends UUIDColumn(this) with PartitionKey[UUID]
-  object name extends StringColumn(this)
-
-  def fromRow(r: Row): StubRecord = {
-    StubRecord(name(r), id(r))
-  }
+import com.newzly.phantom.tables.{
+  TableWithCompositeKey,
+  TableWithCompoundKey,
+  TableWithNoKey,
+  TableWithSingleKey
 }
-
-object TableWithSingleKey extends TableWithSingleKey
-
-class TableWithCompoundKey extends CassandraTable[TableWithCompoundKey, StubRecord] {
-
-  object id extends UUIDColumn(this) with PartitionKey[UUID]
-  object second extends UUIDColumn(this) with PrimaryKey[UUID]
-  object name extends StringColumn(this)
-
-  def fromRow(r: Row): StubRecord = {
-    StubRecord(name(r), id(r))
-  }
-}
-
-object TableWithCompoundKey extends TableWithCompoundKey
-
-
-
-sealed class TableWithCompositeKey extends CassandraTable[TableWithCompositeKey, StubRecord] {
-
-  object id extends UUIDColumn(this) with PartitionKey[UUID]
-  object second_part extends UUIDColumn(this) with PartitionKey[UUID]
-  object second extends UUIDColumn(this) with PrimaryKey[UUID]
-  object name extends StringColumn(this)
-
-  def fromRow(r: Row): StubRecord = {
-    StubRecord(name(r), id(r))
-  }
-}
-
-object TableWithCompositeKey extends TableWithCompositeKey
-
-sealed class TableWithNoKey extends CassandraTable[TableWithNoKey, StubRecord] {
-
-  object id extends UUIDColumn(this)
-  object name extends StringColumn(this)
-
-  def fromRow(r: Row): StubRecord = {
-    StubRecord(name(r), id(r))
-  }
-}
-
-object TableWithNoKey extends TableWithNoKey
-
 
 class TableKeyGenerationTest extends FlatSpec with Matchers with ParallelTestExecution {
 
@@ -75,7 +23,7 @@ class TableKeyGenerationTest extends FlatSpec with Matchers with ParallelTestExe
   }
 
   it should "correctly create a Composite key from a table with a two Partition keys and one Primary key" in {
-    TableWithCompoundKey.defineTableKey() shouldEqual s"PRIMARY_KEY ((id, second_part), second)"
+    TableWithCompositeKey.defineTableKey() shouldEqual s"PRIMARY_KEY ((id, second_part), second)"
   }
 
 
