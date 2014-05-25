46a9f86bbff5485568dd451cd66329515dd34bae
Flavian Alexander <alexandruflavian23@gmail.com>
2014-05-25 22:00:12 +0100

    PHANTOM-79: Added ability to chain multiple CAS clauses to a single query.

--- phantom-test/src/test/scala/com/newzly/phantom/dsl/specialized/ConditionalQueries.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/dsl/specialized/ConditionalQueries.scala
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2013 newzly ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.newzly.phantom.dsl.specialized
 
 import scala.concurrent.blocking
-import com.datastax.driver.core.exceptions.InvalidQueryException
 import com.datastax.driver.core.utils.UUIDs
 import com.newzly.phantom.Implicits._
 import com.newzly.phantom.tables.{ Recipe, Recipes }
@@ -104,7 +118,7 @@ class ConditionalQueries extends BaseTest {
     }
   }
 
-  it should "throw an error when a list column is used a conditional clause" in {
+  it should "execute an update when a list column is used a conditional clause" in {
 
     val recipe = Recipe.sample
     val id = UUIDs.timeBased()
@@ -141,12 +155,55 @@ class ConditionalQueries extends BaseTest {
         second.isDefined shouldEqual true
 
         info("And it should contain the updated value of the uid")
-        second.get.description shouldEqual(updated)
+        second.get.description shouldEqual updated
       }
     }
   }
 
-  it should "throw an error when a list column is used a conditional clause with Twitter Futures" in {
+  it should "not execute the update when the list column in a conditional clause doesn't match" in {
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+
+    val invalidMatch = List("invalid1", "invalid2")
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .future()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).one()
+      update <- Recipes.update.where(_.url eqs recipe.url).modify(_.description setTo updated).onlyIf(_.ingredients eqs invalidMatch).future()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).one()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it shouldn't have updated the value")
+        second.get.description shouldNot equal(updated)
+      }
+    }
+  }
+
+  it should "execute an update when a list column is used a conditional clause with Twitter Futures" in {
 
     val recipe = Recipe.sample
     val id = UUIDs.timeBased()
@@ -183,7 +240,50 @@ class ConditionalQueries extends BaseTest {
         second.isDefined shouldEqual true
 
         info("And it should contain the updated value of the uid")
-        second.get.description shouldEqual(updated)
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+  it should "not execute the update when the list column in a conditional clause doesn't match with Twitter Futures" in {
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+
+    val invalidMatch = List("invalid1", "invalid2")
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .execute()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).get()
+      update <- Recipes.update.where(_.url eqs recipe.url).modify(_.description setTo updated).onlyIf(_.ingredients eqs invalidMatch).execute()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).get()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it shouldn't have updated the value")
+        second.get.description shouldNot equal(updated)
       }
     }
   }
@@ -271,4 +371,369 @@ class ConditionalQueries extends BaseTest {
       }
     }
   }
+
+  it should "execute an update with a multi-part CAS conditional query with no collection columns in the CAS part" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .future()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).one()
+      update <- Recipes.update.where(_.url eqs recipe.url).modify(_.description setTo updated).onlyIf(_.description eqs recipe.description).and(_.uid eqs id).future()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).one()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+  it should "execute an update with a multi-part CAS conditional query with no collection columns in the CAS part with Twitter Futures" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .execute()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).get()
+      update <- Recipes.update.where(_.url eqs recipe.url).modify(_.description setTo updated).onlyIf(_.description eqs recipe.description).and(_.uid eqs id).execute()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).get()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+  it should "execute an update with a tri-part CAS conditional query with no collection columns in the CAS part" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .future()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).one()
+      update <- Recipes.update.where(_.url eqs recipe.url)
+        .modify(_.description setTo updated)
+        .onlyIf(_.description eqs recipe.description)
+        .and(_.last_checked_at eqs recipe.lastCheckedAt)
+        .and(_.uid eqs id).future()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).one()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+  it should "execute an update with a tri-part CAS conditional query with no collection columns in the CAS part with Twitter Futures" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .execute()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).get()
+      update <- Recipes.update.where(_.url eqs recipe.url)
+        .modify(_.description setTo updated)
+        .onlyIf(_.description eqs recipe.description)
+        .and(_.last_checked_at eqs recipe.lastCheckedAt)
+        .and(_.uid eqs id).execute()
+
+      select2 <- Recipes.select.where(_.url eqs recipe.url).get()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+  it should "execute an update with a dual-part CAS conditional query with a mixture of collection columns in the CAS part" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .future()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).one()
+      update <- Recipes.update.where(_.url eqs recipe.url)
+        .modify(_.description setTo updated)
+        .onlyIf(_.props eqs recipe.props)
+        .and(_.ingredients eqs recipe.ingredients)
+        .future()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).one()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+  it should "execute an update with a dual-part CAS conditional query with a mixture of collection columns in the CAS part with Twitter Futures" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .execute()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).get()
+      update <- Recipes.update.where(_.url eqs recipe.url)
+        .modify(_.description setTo updated)
+        .onlyIf(_.props eqs recipe.props)
+        .and(_.ingredients eqs recipe.ingredients)
+        .execute()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).get()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+  it should "execute an update with a dual-part CAS conditional query with a mixture of collection columns and simple comparisons in the CAS part" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .future()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).one()
+      update <- Recipes.update.where(_.url eqs recipe.url)
+        .modify(_.description setTo updated)
+        .onlyIf(_.props eqs recipe.props)
+        .and(_.uid eqs id)
+        .and(_.ingredients eqs recipe.ingredients)
+        .future()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).one()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
+
+  it should "execute an update with a dual-part CAS query with a mixture of columns with Twitter Futures" in {
+
+    val recipe = Recipe.sample
+    val id = UUIDs.timeBased()
+    val updated = Some(Sampler.getARandomString)
+
+    val insert = Recipes.insert
+      .value(_.uid, id)
+      .value(_.url, recipe.url)
+      .value(_.description, recipe.description)
+      .value(_.ingredients, recipe.ingredients)
+      .value(_.last_checked_at, recipe.lastCheckedAt)
+      .value(_.props, recipe.props)
+      .execute()
+
+    val chain = for {
+      insert <- insert
+      select1 <- Recipes.select.where(_.url eqs recipe.url).get()
+      update <- Recipes.update.where(_.url eqs recipe.url)
+        .modify(_.description setTo updated)
+        .onlyIf(_.props eqs recipe.props)
+        .and(_.uid eqs id)
+        .and(_.ingredients eqs recipe.ingredients)
+        .execute()
+      select2 <- Recipes.select.where(_.url eqs recipe.url).get()
+    } yield (select1, select2)
+
+    chain.successful {
+      res => {
+        val initial = res._1
+        val second = res._2
+
+        info("The first record should not be empty")
+        initial.isDefined shouldEqual true
+
+        info("And it should match the inserted values")
+        initial.get.url shouldEqual recipe.url
+
+        info("The updated record should not be empty")
+        second.isDefined shouldEqual true
+
+        info("And it should contain the updated value of the uid")
+        second.get.description shouldEqual updated
+      }
+    }
+  }
+
 }
