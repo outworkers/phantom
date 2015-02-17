0a6fed962a8c25fdef4dc52938af04a3616a8e6a
Flavian Alexandru <alexandruflavian23@gmail.com>
2015-02-17 15:01:51 +0000

    Removing zk instance from phantom

--- phantom-zookeeper/src/main/scala/com/websudos/phantom/zookeeper/ZookeeperInstance.scala
+++ phantom-zookeeper/src/main/scala/com/websudos/phantom/zookeeper/ZookeeperInstance.scala
@@ -44,7 +44,7 @@ import com.twitter.finagle.exp.zookeeper.ZooKeeper
 import com.twitter.finagle.zookeeper.ZookeeperServerSetCluster
 import com.twitter.util.{Await, Future, RandomSocket, Try}
 
-class ZookeeperInstance(val address: InetSocketAddress = RandomSocket.nextAddress()) {
+class ookeeperInstance(val address: InetSocketAddress = RandomSocket.nextAddress()) {
 
   private[this] val status = new AtomicBoolean(false)
 
