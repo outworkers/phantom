a93f9e399c32852ecf152d753cefbfa975f68d1e
Flavian Alexandru <alexandruflavian23@gmail.com>
2015-03-11 21:10:23 +0000

    Adding all missing tests.

--- phantom-dsl/src/test/scala/com/websudos/phantom/builder/query/CreateQueryTest.scala
+++ phantom-dsl/src/test/scala/com/websudos/phantom/builder/query/CreateQueryTest.scala
@@ -1,10 +1,13 @@
 package com.websudos.phantom.builder.query
 
-import scala.concurrent.duration._
 import com.twitter.conversions.storage._
+import com.twitter.util.{Duration => TwitterDuration}
 import com.websudos.phantom.dsl._
 import com.websudos.phantom.tables.BasicTable
+import org.joda.time.Seconds
 import org.scalatest.{FreeSpec, Matchers}
+
+import scala.concurrent.duration._
 
 class CreateQueryTest extends FreeSpec with Matchers {
 
@@ -54,6 +57,16 @@ class CreateQueryTest extends FreeSpec with Matchers {
 
       "allow specifying larger custom units as gc_grace_seconds" in {
         val qb = BasicTable.newCreate.`with`(gc_grace_seconds eqs 1.day).qb.queryString
+        qb shouldEqual "CREATE TABLE BasicTable (id uuid, id2 uuid, id3 uuid, placeholder text, PRIMARY KEY (id, id2, id3)) WITH gc_grace_seconds = 86400"
+      }
+
+      "allow specifying larger custom units as gc_grace_seconds using the Twitter conversions API" in {
+        val qb = BasicTable.newCreate.`with`(gc_grace_seconds eqs TwitterDuration.fromSeconds(86400)).qb.queryString
+        qb shouldEqual "CREATE TABLE BasicTable (id uuid, id2 uuid, id3 uuid, placeholder text, PRIMARY KEY (id, id2, id3)) WITH gc_grace_seconds = 86400"
+      }
+
+      "allow specifying custom gc_grade_seconds using the Joda Time ReadableInstant and Second API" in {
+        val qb = BasicTable.newCreate.`with`(gc_grace_seconds eqs Seconds.seconds(86400)).qb.queryString
         qb shouldEqual "CREATE TABLE BasicTable (id uuid, id2 uuid, id3 uuid, placeholder text, PRIMARY KEY (id, id2, id3)) WITH gc_grace_seconds = 86400"
       }
     }
