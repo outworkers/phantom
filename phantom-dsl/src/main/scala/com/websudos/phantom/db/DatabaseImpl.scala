72198b643eb1e55860aea0b07f8337ecd7285d0e
Flavian Alexandru <alexandruflavian23@gmail.com>
2015-08-02 09:52:34 +0200

    Forwarding implicit sessions and keyspaces directly from the connector.

--- phantom-dsl/src/main/scala/com/websudos/phantom/db/DatabaseImpl.scala
+++ phantom-dsl/src/main/scala/com/websudos/phantom/db/DatabaseImpl.scala
@@ -38,10 +38,8 @@ abstract class DatabaseImpl(protected[this] val connector: KeySpaceDef) {
     columnMembers.foreach {
       symbol =>
         val table =  if (symbol.isModule) {
-          Console.println(symbol.name.decodedName.toString)
           instanceMirror.reflectModule(symbol.asModule).instance
         } else if (symbol.isTerm){
-          Console.println(symbol.name.decodedName.toString)
           instanceMirror.reflectField(symbol.asTerm).get
         }
         _tables += table.asInstanceOf[CassandraTable[_, _]]
