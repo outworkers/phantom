b7d09f250c392d57f4d15e5ab501162d27f696ac
Flavian Alexandru <alexandruflavian23@gmail.com>
2015-08-16 18:39:53 +0200

    Fixed assertions for lower protocol versions.

--- phantom-dsl/src/test/scala/com/websudos/phantom/builder/query/db/specialized/ConsistencyLevelTests.scala
+++ phantom-dsl/src/test/scala/com/websudos/phantom/builder/query/db/specialized/ConsistencyLevelTests.scala
@@ -1,11 +1,14 @@
 package com.websudos.phantom.builder.query.db.specialized
 
+import com.datastax.driver.core.ProtocolVersion
 import com.websudos.phantom.tables.{Primitives, Primitive}
 import com.websudos.phantom.testkit._
 import com.websudos.util.testing._
 import com.websudos.phantom.dsl._
 
 class ConsistencyLevelTests extends PhantomCassandraTestSuite {
+
+  val protocol = session.getCluster.getConfiguration.getProtocolOptions.getProtocolVersionEnum
 
   it should "set a custom consistency level of ONE" in {
     val row = gen[Primitive]
@@ -64,7 +67,11 @@ class ConsistencyLevelTests extends PhantomCassandraTestSuite {
     val st = Primitives.select.where(_.pkey eqs row.pkey)
       .consistencyLevel_=(ConsistencyLevel.EACH_QUORUM).statement
 
-    st.getConsistencyLevel shouldEqual ConsistencyLevel.EACH_QUORUM
+    if (protocol.compareTo(ProtocolVersion.V2) == 1) {
+      st.getConsistencyLevel shouldEqual ConsistencyLevel.EACH_QUORUM
+    } else {
+      st.getConsistencyLevel shouldEqual null
+    }
   }
 
   it should "set a custom consistency level of LOCAL_ONE in an UPDATE query" in {
@@ -73,7 +80,12 @@ class ConsistencyLevelTests extends PhantomCassandraTestSuite {
     val st = Primitives.update.where(_.pkey eqs row.pkey)
       .consistencyLevel_=(ConsistencyLevel.LOCAL_ONE).statement
 
-    st.getConsistencyLevel shouldEqual ConsistencyLevel.LOCAL_ONE
+    if (protocol.compareTo(ProtocolVersion.V2) == 1) {
+      st.getConsistencyLevel shouldEqual ConsistencyLevel.LOCAL_ONE
+    } else {
+      st.getConsistencyLevel shouldEqual null
+    }
+
   }
 
   it should "set a custom consistency level of QUORUM in an INSERT query" in {
@@ -81,7 +93,11 @@ class ConsistencyLevelTests extends PhantomCassandraTestSuite {
 
     val st = Primitives.store(row).consistencyLevel_=(ConsistencyLevel.QUORUM).statement
 
-    st.getConsistencyLevel shouldEqual ConsistencyLevel.QUORUM
+    if (protocol.compareTo(ProtocolVersion.V2) == 1) {
+      st.getConsistencyLevel shouldEqual ConsistencyLevel.QUORUM
+    } else {
+      st.getConsistencyLevel shouldEqual null
+    }
   }
 
   it should "set a custom consistency level of QUORUM in a TRUNCATE query" in {
@@ -94,7 +110,11 @@ class ConsistencyLevelTests extends PhantomCassandraTestSuite {
     val st = Primitives.create.ifNotExists()
       .consistencyLevel_=(ConsistencyLevel.LOCAL_QUORUM).statement
 
-    st.getConsistencyLevel shouldEqual ConsistencyLevel.LOCAL_QUORUM
+    if (protocol.compareTo(ProtocolVersion.V2) == 1) {
+      st.getConsistencyLevel shouldEqual ConsistencyLevel.LOCAL_QUORUM
+    } else {
+      st.getConsistencyLevel shouldEqual null
+    }
   }
 
 }
