63267ba00900c74b2b0362395956866a39fea2b4
Flavian Alexandru <flavian.alexandru@anomaly42.com>
2015-11-19 21:14:08 +0000

    Adding streams.

--- phantom-reactivestreams/src/test/scala/com/websudos/phantom/reactivestreams/suites/PublisherIntegrationTest.scala
+++ phantom-reactivestreams/src/test/scala/com/websudos/phantom/reactivestreams/suites/PublisherIntegrationTest.scala
@@ -1,3 +1,32 @@
+/*
+ * Copyright 2013-2015 Websudos, Limited.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * - Explicit consent must be obtained from the copyright owner, Websudos Limited before any redistribution is made.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.websudos.phantom.reactivestreams.suites
 
 import java.util.concurrent.atomic.AtomicInteger
@@ -7,11 +36,14 @@ import org.scalatest.FlatSpec
 import com.websudos.util.testing._
 import com.websudos.phantom.dsl._
 import com.websudos.phantom.reactivestreams._
+import org.scalatest.concurrent.Eventually
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class PublisherIntegrationTest extends FlatSpec with StreamTest with TestImplicits {
+class PublisherIntegrationTest extends FlatSpec with StreamTest with TestImplicits with Eventually {
+
+  override implicit val patienceConfig = PatienceConfig(5.seconds, 400.millis)
 
   it should "correctly consume the entire stream of items published from a Cassandra table" in {
     val counter = new AtomicInteger(0)
@@ -36,7 +68,9 @@ class PublisherIntegrationTest extends FlatSpec with StreamTest with TestImplici
         fail(t)
       }
 
-      override def onSubscribe(s: Subscription): Unit = ()
+      override def onSubscribe(s: Subscription): Unit = {
+        s.request(Long.MaxValue)
+      }
 
       override def onComplete(): Unit = {
         info(s"Finished streaming, total count is ${counter.get()}")
@@ -47,6 +81,12 @@ class PublisherIntegrationTest extends FlatSpec with StreamTest with TestImplici
         info(s"The current count is ${counter.incrementAndGet()}")
       }
     })
+
+
+    eventually {
+      counter.get() shouldEqual generatorCount
+    }
+
 
   }
 }
