e5d19f12013d4fb38822a4976ceb11480e2c895f
Flavian Alexandru <flavian.alexandru@anomaly42.com>
2016-01-04 10:04:05 +0000

    Adding more copyright info missing and session augmenting for missing methods.

--- phantom-connectors/src/main/scala/com/websudos/phantom/connectors/Connector.scala
+++ phantom-connectors/src/main/scala/com/websudos/phantom/connectors/Connector.scala
@@ -29,7 +29,7 @@
  */
 package com.websudos.phantom.connectors
 
-import com.datastax.driver.core.{ProtocolVersion, Session}
+import com.datastax.driver.core.Session
 
 /* Trait to be mixed into the table classes that extend
  * phantom's CassandraTable.
