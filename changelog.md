2359c4ca2e372f84d12e2dc4299f61ade7bf538d
Flavian Alexandru <flavian.alexandru@anomaly42.com>
2016-01-17 13:36:35 +0000

    Adding tests for time functions

--- changelog.md
+++ changelog.md
@@ -30,7 +30,7 @@ Changelog
     <li><a href="#version-1.18.1">1.18.1 - 17.12.2015</a></li>
     <li><a href="#version-1.19.0">1.19.1 - 04.01.2016</a></li>
     <li><a href="#version-1.20.0">1.20.0 - 08.01.2016</a></li>
-    <li><a href="#version-1.21.0">1.21.0 - 15.01.2016</a></li>
+    <li><a href="#version-1.21.0">1.21.0 - 18.01.2016</a></li>
 </ul>
 
 
@@ -303,8 +303,12 @@ and `org.joda.time.DateTime`.
 <a id="version-1.21.0">1.21.0</a>
 ================================
 
-- Added support for preparing `UpdateQuery` via `p_where`, `p_and`, `p_modify` and `p_ttl`.
+- Added support for preparing `UpdateQuery` via `p_where` -> `p_and` pairs and `p_modify` -> `p_and` pairs.
 - Added more tests for prepared delete queries and update queries.
 - Added the ability to set more options on a statement before executing it.
 - Added ability to prepare map entry update clauses.
 - Bumped util library version to `0.10.8`.
+- Bumped Datastax Java Driver version to `3.0.0-rc1` and using `new SimpleStatement` constructor in favour the now
+removed `session.newSimpleStatement`.
+- Added ability to modify a `statement` directly the execution operation to set options via the Java DSL.
+- Added support for `Table.select.function(t => writetime(t.column)`, including `dateOf`, `now` and `writetime`.
---
+++ phantom-dsl/src/test/scala/com/websudos/phantom/builder/query/db/specialized/SelectFunctionsTesting.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013-2015 Websudos, Limited.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * - Explicit consent must be obtained from the copyright owner, Websudos Limited before any redistribution is made.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.websudos.phantom.builder.query.db.specialized
+
+import com.websudos.phantom.PhantomSuite
+import com.websudos.phantom.tables.Recipe
+import com.websudos.phantom.dsl._
+import com.websudos.util.testing._
+
+class SelectFunctionsTesting extends PhantomSuite {
+
+  it should "retrieve the writetime of a field from Cassandra" in {
+    val record = gen[Recipe]
+
+    val chain = for {
+      store <- database.recipes.store(record).future()
+      timestamp <- database.recipes.select.function(t => writetime(t.description))
+        .where(_.url eqs record.url).one()
+    } yield timestamp
+
+    whenReady(chain) {
+      res => {
+        res shouldBe defined
+        shouldNotThrow {
+          info(res.value.toString)
+          // new DateTime(res.value)
+        }
+      }
+    }
+
+  }
+
+
+}
