9f04cbc3ae6440ea0255afb0d8fa0530204a1918
Flavian Alexandru <flavian.alexandru@anomaly42.com>
2016-03-10 14:49:08 +0000

    Adding github push capability.

--- build/publish_develop.sh
+++ build/publish_develop.sh
@@ -2,24 +2,26 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ];
 then
 
-    echo "The current JDK version is $TRAVIS_JDK_VERSION"
-    echo "The current Scala version is $TRAVIS_SCALA_VERSION"
+    echo "The current JDK version is ${TRAVIS_JDK_VERSION}"
+    echo "The current Scala version is ${TRAVIS_SCALA_VERSION}"
 
     CURRENT_VERSION = "$(sbt version)"
-    echo "Bumping release version with a patch increment from $CURRENT_VERSION"
+    echo "Bumping release version with a patch increment from ${CURRENT_VERSION}"
     sbt version-bump-patch
 
     NEW_VERSION = "$(sbt version)"
-    echo "Creating Git tag for version $NEW_VERSION"
+    echo "Creating Git tag for version ${NEW_VERSION}"
 
     echo "Pushing tag to GitHub."
-    git push --tags
+    git push --tags --quiet "https://${github_token}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
     echo "Publishing signed artifact"
     sbt bintray:publish
 
     git checkout master
     git merge develop
+
+    git push --all --quiet "https://${github_token}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
 
 else
