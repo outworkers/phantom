31bee88cda4d2c183abd89f01aaf12fec16f89d7
Flavian Alexandru <flavian@outworkers.com>
2016-07-26 10:22:43 +0100

    Small fix.

--- phantom-dsl/src/test/scala/com/websudos/phantom/tables/TimeSeriesTable.scala
+++ phantom-dsl/src/test/scala/com/websudos/phantom/tables/TimeSeriesTable.scala
@@ -34,6 +34,8 @@ import com.websudos.phantom.builder.query.InsertQuery
 import com.websudos.phantom.dsl._
 import org.joda.time.DateTime
 
+import scala.concurrent.Future
+
 case class TimeSeriesRecord(
   id: UUID,
   name: String,
