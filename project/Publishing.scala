9eb0337f190a1376c925e818c010d61cce9bda19
Dragos Manolescu <coder@micro-workflow.com>
2016-11-15 09:07:21 -0800

    Fixed type on provider instance variable

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -37,7 +37,7 @@ import scala.util.Properties
 object Publishing {
 
   val defaultPublishingSettings = Seq(
-    version := "1.29.5"
+    version := "1.29.6-SNAPSHOT"
   )
 
   lazy val noPublishSettings = Seq(
