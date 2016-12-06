cd5f55c5ba221967b01994b38552ec761ba2e16f
Flavian Alexandru <flavian@outworkers.com>
2016-12-06 14:47:27 +0000

    Adding macro inference

--- phantom-dsl/src/main/scala/com/outworkers/phantom/macros/TableHelper.scala
+++ phantom-dsl/src/main/scala/com/outworkers/phantom/macros/TableHelper.scala
@@ -20,6 +20,7 @@ import com.outworkers.phantom.SelectTable
 import com.outworkers.phantom.column.{AbstractColumn, PrimitiveColumn}
 import com.outworkers.phantom.dsl.CassandraTable
 
+import scala.collection.immutable.ListSet
 import scala.reflect.macros.blackbox
 
 trait TableHelper[T <: CassandraTable[T, R], R] {
@@ -74,48 +75,100 @@ class TableHelperMacro(override val c: blackbox.Context) extends MacroUtils(c) {
     })
   }
 
+  /**
+    * Materializes an extractor method for a table, the so called "fromRow" method.
+    *
+    * This will only work if the types of the [[R]] record type match the types
+    * inferred by the return types of the columns inside the table.
+    *
+    * If the implementation could not be inferred, the output of this method will be the unimplemented
+    * method exception and the user will have to manually override the fromRow definition and create one
+    * themselves.
+    *
+    * {{{
+    *   def fromRow(row: Row): R = ???
+    * }}}
+    *
+    * Not only that but they also have to be in the same order. For example:
+    * {{{
+    *   case class MyRecord(
+    *     id: UUID,
+    *     email: String,
+    *     date: DateTime
+    *   )
+    *
+    *   class MyTable extends CassandraTable[MyTable, MyRecord] {
+    *     object id extends UUIDColumn(this) with PartitionKey
+    *     object email extends StringColumn(this)
+    *     object date extends DateTimeColumn(this)
+    *   }
+    * }}}
+    *
+    * @tparam T The type of the Cassandra table to infer the extractor for.
+    * @tparam R The record type to infer the extractor for.
+    * @return An interpolated tree that will contain the automatically generated implementation
+    *         of the fromRow method in a Cassandra Table.
+    *         Alternatively, this will return an unimplemented ??? method, provided a correct
+    *         definition could not be inferred.
+    */
   def materializeExtractor[T : WeakTypeTag, R : WeakTypeTag]: Tree = {
     val tableTpe = weakTypeOf[T]
     val recordTpe = weakTypeOf[R]
+
+    val rowTerm = TermName("row")
+
     val sourceMembers = filterMembers[T, AbstractColumn[_]](exclusions)
+    val columnNames = sourceMembers.map(
+      tpe => {
+        q"${tpe.typeSignatureIn(tableTpe).typeSymbol.name.toTermName}.apply($rowTerm)"
+      }
+    )
 
-    Console.println(sourceMembers)
+    /**
+      * First we create a set of ordered types corresponding to the type signatures
+      * found in the case class arguments.
+      */
+    val recordMembers = fields(recordTpe) map (_._2)
 
-
-    val columns = sourceMembers.map(_.asModule.info)
-    val records = fields(recordTpe) map (_._2) toSet
-
+    /**
+      * Then we do the same for the columns, we filter for the members of [[T]] that
+      * directly subclass [[AbstractColumn[_]]. For every one of those methods, we
+      * are going to look at what type argumnt was passed by the specific column definition
+      * when extending [[AbstractColumn[_]] as this will tell us the Scala output type
+      * of the given column.
+      * We create a list of these types and if they match the case class types expected,
+      * it means we can auto-generate a fromRow implementation.
+      */
     val colMembers = sourceMembers.map { member =>
       val memberType = member.typeSignatureIn(tableTpe)
 
       memberType.baseClasses.find(colSymbol ==) match {
-        case Some(root) => {
-
-          println(root.asType.typeSignature.widen.dealias.typeArgs)
-
-          root.typeSignatureIn(memberType).typeArgs.headOption match {
-            case Some(colSignature) => colSignature
-            case None => c.abort(
-              c.enclosingPosition,
-              s"Could not find the type argument passed to AbstractColumn for ${member.asModule.name}"
-            )
+        case Some(root) =>
+          // Here we expect to have a single type argument or type param
+          // because we know root here will point to an AbstractColumn[_] symbol.
+          root.typeSignature.typeParams match {
+            // We use the special API to see what type was passed through to AbstractColumn[_]
+            // with special thanks to https://github.com/joroKr21 for helping me not rip
+            // the remainder of my hair off while uncovering this marvelous macro API method.
+            case head :: Nil => head.asType.toType.asSeenFrom(memberType, colSymbol)
+            case _ => c.abort(c.enclosingPosition, "Expected exactly one type parameter provided for root column type")
           }
-        }
         case None => c.abort(c.enclosingPosition, s"Could not find root column type for ${member.asModule.name}")
       }
     }
 
-    Console.println("The column types in the type tree")
-    Console.println(colMembers.map(_.typeSymbol.name.decodedName.toString).mkString("\n"))
-    Console.println("The records types in the type tree")
-    Console.println(records.map(_.typeSymbol.name.decodedName.toString).mkString("\n"))
+    val recordMembersSet = recordMembers.to[ListSet]
 
-    val methodTree = q"""
-      null.asInstanceOf[$recordTpe]
-    """
+    Console.println(s"The ${recordMembersSet.size} case class types")
+    Console.println(recordMembersSet.map(_.typeSymbol.fullName).mkString("\n"))
 
-    println(showCode(methodTree))
-    methodTree
+    Console.println(s"The ${colMembers.size} output types inferred from the schema definition")
+    Console.println(colMembers.map(_.typeSymbol.fullName).mkString("\n"))
+
+    val tree = q"""new ${recordTpe}(..$columnNames)"""
+
+    println(showCode(tree))
+    tree
   }
 
 
@@ -153,9 +206,7 @@ class TableHelperMacro(override val c: blackbox.Context) extends MacroUtils(c) {
        new com.outworkers.phantom.macros.TableHelper[$tpe, $rTpe] {
           def tableName: $strTpe = $finalName
 
-          def fromRow(row: $rowType): $rTpe = {
-            return ${materializeExtractor[T, R]}
-          }
+          def fromRow(row: $rowType): $rTpe = ${materializeExtractor[T, R]}
 
           def fields(table: $tableTpe): scala.collection.immutable.Set[$colTpe] = {
             scala.collection.immutable.Set.apply[$colTpe](..$accessors)
