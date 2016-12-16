1c2556b158d1d48a514e5998bca4548fe9323420
Flavian Alexandru <flavian@outworkers.com>
2016-12-16 12:11:36 +0000

    Running setup for scala 2.11.8 instead

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-if [ "${TRAVIS_SCALA_VERSION}" == "2.12.0" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
+if [ "${TRAVIS_SCALA_VERSION}" == "2.11.8" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
 then
     echo "Running tests with coverage and report submission"
-    sbt "plz $TRAVIS_SCALA_VERSION coverage; plz $TRAVIS_SCALA_VERSION test; plz $TRAVIS_SCALA_VERSION coverageReport; plz $TRAVIS_SCALA_VERSION coverageAggregate; plz $TRAVIS_SCALA_VERSION coveralls;"
+    sbt "plz $TRAVIS_SCALA_VERSION coverage test coverageReport coverageAggregate coveralls"
     exit $?
 else
     echo "Running tests without attempting to submit coverage reports"
