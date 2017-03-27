5295ac7bf021d73dedcc7ffeec71df3ec3a93f7f
Flavian Alexandru <flavian@outworkers.com>
2017-03-27 10:35:30 +0100

    Bumping version and improving the extractor [version skip]

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -23,7 +23,7 @@ import scala.util.Properties
 object Publishing {
 
   val defaultPublishingSettings = Seq(
-    version := "2.5.0"
+    version := "2.6.0"
   )
 
   lazy val noPublishSettings = Seq(
