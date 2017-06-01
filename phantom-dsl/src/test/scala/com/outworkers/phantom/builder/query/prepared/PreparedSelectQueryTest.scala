79741868ecebdbac070151237de3b4bc3306f83d
Flavian Alexandru <flavian@outworkers.com>
2017-06-01 22:01:01 +0100

    Removing unused imports.

--- phantom-dsl/src/test/scala/com/outworkers/phantom/builder/query/prepared/PreparedSelectQueryTest.scala
+++ phantom-dsl/src/test/scala/com/outworkers/phantom/builder/query/prepared/PreparedSelectQueryTest.scala
@@ -17,10 +17,8 @@ package com.outworkers.phantom.builder.query.prepared
 
 import com.outworkers.phantom.PhantomSuite
 import com.outworkers.phantom.dsl._
-import com.outworkers.phantom.macros.SingleGeneric
 import com.outworkers.phantom.tables._
 import com.outworkers.util.samplers._
-import shapeless.{ HNil, :: }
 
 class PreparedSelectQueryTest extends PhantomSuite {
 
