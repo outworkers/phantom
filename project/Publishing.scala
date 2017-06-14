efdb285661ec8a7be333eafa6ec615c9ffa50bcd
Flavian Alexandru <flavian@outworkers.com>
2017-06-14 19:50:40 +0100

    Bumping version to 2.11.0 [version skip]

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -23,7 +23,7 @@ import scala.util.Properties
 object Publishing {
 
   val defaultPublishingSettings = Seq(
-    version := "2.10.0"
+    version := "2.11.0"
   )
 
   lazy val noPublishSettings = Seq(
