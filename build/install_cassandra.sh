4beab7d3200ea31d3fe8c292893a17feec0dc96b
Flavian Alexandru <flavian@outworkers.com>
2017-07-07 13:28:47 +0100

    Bumping Cassandra version

--- build/install_cassandra.sh
+++ build/install_cassandra.sh
@@ -29,7 +29,7 @@ jdk_version_8_or_more=$(check_java_version)
 
 if [ "$jdk_version_8_or_more" > 1 ];
   then
-    cassandra_version="3.8"
+    cassandra_version="3.11"
   else
     cassandra_version="2.2.9"
 fi
