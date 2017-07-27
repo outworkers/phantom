68820e76bd35c4dfd72a9e60cac28d6b74729b7e
Flavian Alexandru <flavian@outworkers.com>
2017-07-27 10:16:08 +0100

    Null counter columns should not cause errors

--- phantom-dsl/src/main/scala/com/outworkers/phantom/column/CounterColumn.scala
+++ phantom-dsl/src/main/scala/com/outworkers/phantom/column/CounterColumn.scala
@@ -15,11 +15,11 @@
  */
 package com.outworkers.phantom.column
 
-import com.outworkers.phantom.{ CassandraTable, Row }
 import com.outworkers.phantom.builder.primitives.Primitive
 import com.outworkers.phantom.builder.query.engine.CQLQuery
 import com.outworkers.phantom.builder.syntax.CQLSyntax
 import com.outworkers.phantom.keys.Unmodifiable
+import com.outworkers.phantom.{CassandraTable, Row}
 
 import scala.util.Try
 
@@ -35,7 +35,9 @@ class CounterColumn[
 
   override val isCounterColumn = true
 
-  def parse(r: Row): Try[Long] = primitive.fromRow(name, r)
+  def parse(r: Row): Try[Long] = primitive.fromRow(name, r) recover {
+    case e: Exception if r.isNull(name) => 0L
+  }
 
   override def qb: CQLQuery = CQLQuery(name).forcePad.append(cassandraType)
 
