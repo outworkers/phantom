e6f0f9f326b6ef559d9c70666ffcf19b385724a1
Travis CI <ci@outworkers.com>
2017-08-30 12:11:05 +0000

    TravisCI: Bumping version to match CI definition [ci skip]

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -23,7 +23,7 @@ import scala.util.Properties
 object Publishing {
 
   val defaultPublishingSettings = Seq(
-    version := "2.14.4"
+    version := "2.14.5"
   )
 
   lazy val noPublishSettings = Seq(
