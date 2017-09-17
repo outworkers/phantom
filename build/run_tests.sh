fa807b82d3488cd17c14548e42d640eeefd469da
Flavian Alexandru <flavian@outworkers.com>
2017-09-17 16:27:11 -0700

    Running tut compilation after tests.

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
-if [ "${TRAVIS_SCALA_VERSION}" == "2.12.3" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
-then
-    echo "Running tests with coverage and report submission"
-    sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport coverageAggregate coveralls
-    exit $?
-else
-    echo "Running tests without attempting to submit coverage reports or run tut"
-    sbt "plz $TRAVIS_SCALA_VERSION test"
-    exit $?
-fi
+function run_test_suite {
+    if [ "${TRAVIS_SCALA_VERSION}" == "2.12.3" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
+    then
+        echo "Running tests with coverage and report submission"
+        sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport coverageAggregate coveralls
+        testExitCode=$?
+
+        if [ ${testExitCode} == 0 ];
+        then
+            sbt ++$TRAVIS_SCALA_VERSION "project readme" "tut"
+            exit $?
+        else
+            echo "Unable to run tut compilation, test suite failed"
+            exit ${testExitCode}
+        fi
+    else
+        echo "Running tests without attempting to submit coverage reports or run tut"
+        sbt "plz $TRAVIS_SCALA_VERSION test"
+        exit $?
+    fi
+}
+
+run_test_suite
