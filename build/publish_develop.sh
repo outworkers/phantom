c7f9bf38ba048186384f38e8d789da7fe0a45404
Flavian Alexandru <flavian.alexandru.external@ynap.com>
2017-11-28 14:12:44 +0000

    Fixing if statement

--- build/publish_develop.sh
+++ build/publish_develop.sh
@@ -87,21 +87,21 @@ then
         then
             if [[ $COMMIT_MSG == *"$COMMIT_SKIP_MESSAGE"* ]];
             then
-                echo "Publishing new version to Maven Central"
-                echo "Creating GPG deploy key"
-                openssl aes-256-cbc -K $encrypted_759d2b7e5bb0_key -iv $encrypted_759d2b7e5bb0_iv -in build/deploy.asc.enc -out build/deploy.asc -d
-
-                echo "importing GPG key to local GBP repo"
-                gpg --fast-import build/deploy.asc
-
-                echo "Setting MAVEN_PUBLISH mode to true"
-                export MAVEN_PUBLISH="true"
-                export pgp_passphrase=${maven_password}
-                sbt "such publishSigned"
-                sbt sonatypeReleaseAll
-                exit $?
+              echo "Skipping publication of a new Maven Artefact, the version was not bumped."
             else
-                echo "Skipping publication of a new Maven Artefact, the version was not bumped."
+              echo "Publishing new version to Maven Central"
+              echo "Creating GPG deploy key"
+              openssl aes-256-cbc -K $encrypted_759d2b7e5bb0_key -iv $encrypted_759d2b7e5bb0_iv -in build/deploy.asc.enc -out build/deploy.asc -d
+
+              echo "importing GPG key to local GBP repo"
+              gpg --fast-import build/deploy.asc
+
+              echo "Setting MAVEN_PUBLISH mode to true"
+              export MAVEN_PUBLISH="true"
+              export pgp_passphrase=${maven_password}
+              sbt "such publishSigned"
+              sbt sonatypeReleaseAll
+              exit $?
             fi
         else
             echo "Not deploying to Maven Central, branch is not develop, current branch is ${TRAVIS_BRANCH}"
