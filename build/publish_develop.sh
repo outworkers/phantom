b3a13f1f5dea600e9ec706875294b575c499ae13
Flavian Alexandru <flavian.alexandru.external@ynap.com>
2017-11-28 11:56:33 +0000

    Propagating Scala version to trigger coveralls via .yml

--- build/publish_develop.sh
+++ build/publish_develop.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Pull request: ${TRAVIS_PULL_REQUEST}; Branch: ${TRAVIS_BRANCH}"
-TARGET_SCALA_VERSION="2.12.4"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ];
 then
-    if [ "${TRAVIS_SCALA_VERSION}" == "${TARGET_SCALA_VERSION}" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
+    if [ "${TRAVIS_SCALA_VERSION}" == "${TRIGGER_SCALA_VERSION}" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
     then
 
         echo "Setting git user email to ci@outworkers.com"
@@ -117,7 +116,7 @@ then
         fi
 
     else
-        echo "Only publishing version for Scala $TARGET_SCALA_VERSION and Oracle JDK 8 to prevent multiple artifacts"
+        echo "Only publishing version for Scala $TRIGGER_SCALA_VERSION and Oracle JDK 8 to prevent multiple artifacts"
     fi
 else
     echo "This is either a pull request or the branch is not develop, deployment not necessary"
