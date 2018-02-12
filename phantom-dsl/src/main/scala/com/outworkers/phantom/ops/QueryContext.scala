a52c898e4c15929c9ba4c56e90696f961a4d7a01
Flavian Alexandru <flavian@outworkers.com>
2018-02-12 09:55:23 +0000

    Adding other examples.

--- phantom-dsl/src/main/scala/com/outworkers/phantom/ops/QueryContext.scala
+++ phantom-dsl/src/main/scala/com/outworkers/phantom/ops/QueryContext.scala
@@ -38,6 +38,10 @@ abstract class QueryContext[P[_], F[_], Timeout](
   val promiseInterface: PromiseInterface[P, F]
 ) { outer =>
 
+
+  type ListValue[T] = com.outworkers.phantom.builder.query.prepared.ListValue[T]
+  val ListValue = com.outworkers.phantom.builder.query.prepared.ListValue
+
   implicit val adapter: GuavaAdapter[F] = promiseInterface.adapter
 
   def executeStatements[M[X] <: TraversableOnce[X]](
