f61994b5558787a01b4cfe3e2b507a9ad9ec7933
Flavian Alexandru <flavian@outworkers.com>
2018-02-15 14:13:28 +0000

    Reversing the installation order.

--- build/install_cassandra.sh
+++ build/install_cassandra.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-pip install --user 'urllib3'
-pip install --user 'requests'
 pip install --user 'pyOpenSSL'
 pip install --user 'ndg-httpsclient'
+
 pip install --user 'pyasn1'
+pip install --user 'urllib3'
+pip install --user 'requests[security]'
 pip install --user ccm
 ccm create test -v "3.10" -n 1 -s
