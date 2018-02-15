1b14ad7f55bd5887b1bb29175ae3178ee4496241
Flavian Alexandru <flavian@outworkers.com>
2018-02-15 14:52:52 +0000

    Increasing timeout setting.

--- build/install_cassandra.sh
+++ build/install_cassandra.sh
@@ -8,4 +8,4 @@ pip install --user 'pyasn1'
 pip install --user 'urllib3'
 pip install --user 'requests[security]'
 pip install --user ccm
-ccm create test -v "3.10" -n 1 -s
+ccm create test -v "3.10" -n 1 -s timeout 60
