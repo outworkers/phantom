6912386b4bd2426a32fb49f1791e0513562db634
Flavian Alexandru <flavian@outworkers.com>
2018-02-15 13:36:27 +0000

    Add Cassandra install

--- build/install_cassandra.sh
+++ build/install_cassandra.sh
@@ -1,46 +1,9 @@
 #!/usr/bin/env bash
 
-CASSANDRA_VERSION="3.10"
-DEFAULT_CASSANDRA_VERSION="2.2.9"
-
-function check_java_version {
-  if type -p java; then
-    echo found java executable in PATH
-    _java=java
-  elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    echo found java executable in JAVA_HOME
-    _java="$JAVA_HOME/bin/java"
-  else
-    echo "no java"
-    return -1
-  fi
-
-  if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "$version"
-    if [[ ("$version" > "1.8") || ("$version" = "1.8") ]]; then
-      echo "version is more than 1.8"
-      return 1
-    else
-      echo "version is less than 1.8"
-      return -1
-    fi
-  fi
-}
-
-jdk_version_8_or_more=$(check_java_version)
-
-if [ ${jdk_version_8_or_more} -gt 1 ];
-  then
-    cassandra_version="$CASSANDRA_VERSION"
-  else
-    cassandra_version="$DEFAULT_CASSANDRA_VERSION"
-fi
-
-pip install --user 'urllib3[secure]'
+pip install --user 'urllib3'
 pip install --user 'requests'
 pip install --user 'pyOpenSSL'
 pip install --user 'ndg-httpsclient'
 pip install --user 'pyasn1'
 pip install --user ccm
-ccm create test -v ${CASSANDRA_VERSION} -n 1 -s
+ccm create test -v "3.10" -n 1 -s
