98d0841ba2f51bfae7b94c0f3659ea49b01e776a
Flavian Alexandru <flavian@outworkers.com>
2018-05-11 17:11:26 +0100

    Adding ASCII column comparison

--- phantom-dsl/src/test/scala/com/outworkers/phantom/builder/query/db/crud/UpdateQueryTest.scala
+++ phantom-dsl/src/test/scala/com/outworkers/phantom/builder/query/db/crud/UpdateQueryTest.scala
@@ -53,6 +53,7 @@ class UpdateQueryTest extends PhantomSuite with Inside with EitherValues {
         .and(_.date setTo updatedRow.date)
         .and(_.uuid setTo updatedRow.uuid)
         .and(_.bi setTo updatedRow.bi)
+        .and(_.ascii setTo updatedRow.ascii)
         .future()
       a2 <- database.primitives.select.where(_.pkey eqs row.pkey).one
     } yield (a, a2)
