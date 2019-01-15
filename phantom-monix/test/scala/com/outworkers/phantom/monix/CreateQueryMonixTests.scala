bbfd3e50a0a60fabfe12b2b01201bd469af5c3a5
Flavian Alexandru <flavian@outworkers.com>
2019-01-15 14:11:33 +0000

    Adding tests

---
+++ phantom-monix/test/scala/com/outworkers/phantom/monix/CreateQueryMonixTests.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2013 - 2017 Outworkers Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.outworkers.phantom.monix
+
+class CreateQueryMonixTests extends PhantomSuite with MonixScalaTest with TestDatabaseProvider {
+
+  it should "execute a simple query with secondary indexes with Twitter futures" in {
+    whenReady(database.secondaryIndexTable.create.ifNotExists().future()) { res =>
+      info("The creation query of secondary indexes should execute successfully")
+      res.forall(_.wasApplied() == true) shouldEqual true
+    }
+  }
+}
---
+++ phantom-monix/test/scala/com/outworkers/phantom/monix/MonixScalaTest.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 - 2017 Outworkers Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.outworkers.phantom.monix
+
+import monix.eval.Task
+import org.scalatest.concurrent.{ScalaFutures, Waiters}
+
+import scala.util.{Failure, Success}
+import _root_.monix.execution.Scheduler.Implicits.global
+
+trait MonixScalaTest extends Waiters with ScalaFutures {
+
+  implicit def taskFutureConcept[T](f: Task[T]): FutureConcept[T] = new FutureConcept[T] {
+
+    private[this] val source = f.memoize
+
+    override def eitherValue: Option[Either[Throwable, T]] = {
+      source.runAsync.value match {
+        case Some(Success(ret)) => Some(Right(ret))
+        case Some(Failure(err)) => Some(Left(err))
+        case None => None
+      }
+    }
+
+    override def isExpired: Boolean = false
+
+    override def isCanceled: Boolean = false
+  }
+}
