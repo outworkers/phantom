e2bfc8ba34503cfb930e185d0ced828019814297
Flavian Alexandru <flavian@outworkers.com>
2019-02-21 17:45:36 +0000

    Fixing PR run of tut compilation

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -6,15 +6,23 @@ function run_test_suite {
         sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport coverageAggregate coveralls
         test_exit_code=$?
 
-        if [ ${test_exit_code} -eq "0" ];
+        if [ "${TRAVIS_PULL_REQUEST}" == "true" ];
         then
-            echo "Running tut compilation"
-            sbt ++$TRAVIS_SCALA_VERSION "project readme" "tut"
-            local tut_exit_code=$?
-            echo "Tut compilation exited with status $tut_exit_code"
-            exit ${tut_exit_code}
+
+            if [ ${test_exit_code} -eq "0" ];
+                then
+
+                    echo "Running tut compilation"
+                    sbt ++$TRAVIS_SCALA_VERSION "project readme" "tut"
+                    local tut_exit_code=$?
+                    echo "Tut compilation exited with status $tut_exit_code"
+                    exit ${tut_exit_code}
+                else
+                    echo "Unable to run tut compilation, test suite failed"
+                    exit ${test_exit_code}
+                fi
         else
-            echo "Unable to run tut compilation, test suite failed"
+            echo "Not a pull request, no need to run tut compilation as it runs as part of sbt release"
             exit ${test_exit_code}
         fi
     else
