5b3681e2d3933ec0e5284ec6cb3165945ecd41e4
Flavian Alexandru <flavian@outworkers.com>
2019-03-03 20:26:21 +0000

    Reverting alter query changes in wrong branch

--- phantom-dsl/src/main/scala/com/outworkers/phantom/builder/query/AlterQuery.scala
+++ phantom-dsl/src/main/scala/com/outworkers/phantom/builder/query/AlterQuery.scala
@@ -25,17 +25,12 @@ import com.outworkers.phantom.builder.{ConsistencyBound, QueryBuilder, Unspecifi
 import com.outworkers.phantom.column.AbstractColumn
 import com.outworkers.phantom.connectors.KeySpace
 
-case class AlterQuery[
+class AlterQuery[
   Table <: CassandraTable[Table, _],
   Record,
   Status <: ConsistencyBound,
   Chain <: WithBound
-](
-  table: Table,
-  qb: CQLQuery,
-  options: QueryOptions = QueryOptions.empty,
-  alterPart: AlterPart = AlterPart.empty
-) extends RootQuery[Table, Record, Status] {
+](table: Table, val qb: CQLQuery, val options: QueryOptions) extends RootQuery[Table, Record, Status] {
 
   final def add(column: String, columnType: String, static: Boolean = false): AlterQuery[Table, Record, Status, Chain] = {
     val query = if (static) {
@@ -44,15 +39,11 @@ case class AlterQuery[
       QueryBuilder.Alter.add(qb, column, columnType)
     }
 
-    copy(
-      alterPart = alterPart append query
-    )
+    new AlterQuery(table, query, options)
   }
 
   final def add(definition: CQLQuery): AlterQuery[Table, Record, Status, Chain] = {
-    copy (
-      alterPart = alterPart append definition
-    )
+    new AlterQuery(table, QueryBuilder.Alter.add(qb, definition), options)
   }
 
   final def alter[RR](columnSelect: Table => AbstractColumn[RR], newType: String): AlterQuery[Table, Record, Status, Chain] = {
@@ -64,29 +55,29 @@ case class AlterQuery[
   }
 
   /**
-   * Creates an ALTER drop query to drop the column from the schema definition.
-   * It will produce the following type of queries, with the CQL serialization on the right hand side:
-   *
-   * {{{
-   *   MyTable.alter.drop(_.mycolumn) => ALTER TABLE MyTable DROP myColumn
-   * }}}
-   *
-   * @param columnSelect A column selector higher order function derived from a table.
-   * @tparam RR The underlying type of the AbstractColumn.
-   * @return A new alter query with the underlying builder containing a DROP clause.
-   */
+    * Creates an ALTER drop query to drop the column from the schema definition.
+    * It will produce the following type of queries, with the CQL serialization on the right hand side:
+    *
+    * {{{
+    *   MyTable.alter.drop(_.mycolumn) => ALTER TABLE MyTable DROP myColumn
+    * }}}
+    *
+    * @param columnSelect A column selector higher order function derived from a table.
+    * @tparam RR The underlying type of the AbstractColumn.
+    * @return A new alter query with the underlying builder containing a DROP clause.
+    */
   final def drop[RR](columnSelect: Table => DropColumn[RR]): AlterQuery[Table, Record, Status, Chain] = {
     drop(columnSelect(table).column.name)
   }
 
   /**
-   * Creates an ALTER DROP query that drops an entire table.
-   * This is equivalent to table truncation followed by table removal from the keyspace metadata.
-   * This action is irreversible and you should exercise caution is using it.
-   *
-   * @param keySpace The implicit keyspace definition to use.
-   * @return An alter query with a DROP TABLE instruction encoded in the query string.
-   */
+    * Creates an ALTER DROP query that drops an entire table.
+    * This is equivalent to table truncation followed by table removal from the keyspace metadata.
+    * This action is irreversible and you should exercise caution is using it.
+    *
+    * @param keySpace The implicit keyspace definition to use.
+    * @return An alter query with a DROP TABLE instruction encoded in the query string.
+    */
   final def drop()(implicit keySpace: KeySpace): AlterQuery[Table, Record, Status, Chain] = {
     new AlterQuery(table, QueryBuilder.Alter.dropTable(table.tableName, keySpace.name), options)
   }
@@ -96,19 +87,19 @@ case class AlterQuery[
   }
 
   /**
-   * Creates an ALTER drop query to drop the column from the schema definition.
-   * It will produce the following type of queries, with the CQL serialization on the right hand side:
-   *
-   * {{{
-   *   MyTable.alter.drop(_.mycolumn) => ALTER TABLE MyTable DROP myColumn
-   * }}}
-   *
-   * This is used mainly during the autodiffing of schemas, where column selectors are not available
-   * and we only deal with plain string diffs between table metadata collections.
-   *
-   * @param column The string name of the column to drop.
-   * @return A new alter query with the underlying builder containing a DROP clause.
-   */
+    * Creates an ALTER drop query to drop the column from the schema definition.
+    * It will produce the following type of queries, with the CQL serialization on the right hand side:
+    *
+    * {{{
+    *   MyTable.alter.drop(_.mycolumn) => ALTER TABLE MyTable DROP myColumn
+    * }}}
+    *
+    * This is used mainly during the autodiffing of schemas, where column selectors are not available
+    * and we only deal with plain string diffs between table metadata collections.
+    *
+    * @param column The string name of the column to drop.
+    * @return A new alter query with the underlying builder containing a DROP clause.
+    */
   final def drop(column: String): AlterQuery[Table, Record, Status, Chain] = {
     new AlterQuery(table, QueryBuilder.Alter.drop(qb, column), options)
   }
@@ -136,27 +127,27 @@ case class AlterQuery[
 object AlterQuery {
 
   /**
-   * An alias for the default AlterQuery type, available to public accessors.
-   * Allows for simple API usage of type member where a method can pre-set all the phantom types
-   * of all alter query in the background, without leaking them to public APIs.
-   *
-   * {{{
-   *   def alter[T <: CassandraTable[T, _], R]: AlterQuery.Default[T, R]
-   * }}}
-   *
-   * @tparam T The type of the underlying Cassandra table.
-   * @tparam R The type of the record stored in the Cassandra table.
-   */
+    * An alias for the default AlterQuery type, available to public accessors.
+    * Allows for simple API usage of type member where a method can pre-set all the phantom types
+    * of all alter query in the background, without leaking them to public APIs.
+    *
+    * {{{
+    *   def alter[T <: CassandraTable[T, _], R]: AlterQuery.Default[T, R]
+    * }}}
+    *
+    * @tparam T The type of the underlying Cassandra table.
+    * @tparam R The type of the record stored in the Cassandra table.
+    */
   type Default[T <: CassandraTable[T, _], R] = AlterQuery[T, R, Unspecified, WithUnchainned]
 
   /**
-   * The root builder of an ALTER query.
-   * This will initialise the alter builder chain and provide the initial {{{ ALTER $TABLENAME }}} query.
-   * @param table The table to alter.
-   * @tparam T The type of the table.
-   * @tparam R The record held in the table.
-   * @return A raw ALTER query, without any further options set on it.
-   */
+    * The root builder of an ALTER query.
+    * This will initialise the alter builder chain and provide the initial {{{ ALTER $TABLENAME }}} query.
+    * @param table The table to alter.
+    * @tparam T The type of the table.
+    * @tparam R The record held in the table.
+    * @return A raw ALTER query, without any further options set on it.
+    */
   def apply[T <: CassandraTable[T, _], R](table: T)(
     implicit keySpace: KeySpace
   ): AlterQuery.Default[T, R] = {
