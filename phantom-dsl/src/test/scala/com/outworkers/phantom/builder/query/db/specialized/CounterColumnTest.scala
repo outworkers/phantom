0958952cd6c3e359a1b12614e7a5ce84cd2f0183
Flavian Alexandru <flavian@outworkers.com>
2019-04-02 13:17:49 +0100

    Adding tests for prepared counters

--- phantom-dsl/src/test/scala/com/outworkers/phantom/builder/query/db/specialized/CounterColumnTest.scala
+++ phantom-dsl/src/test/scala/com/outworkers/phantom/builder/query/db/specialized/CounterColumnTest.scala
@@ -44,13 +44,34 @@ class CounterColumnTest extends PhantomSuite {
     }
   }
 
+  it should "+= counter values by 1 using prepared statements" in {
+    val sample = gen[CounterRecord]
+
+    val query = database.counterTableTest
+      .update
+      .where(_.id eqs ?)
+      .modify(_.count_entries += ?)
+      .prepareAsync()
+
+    val chain = for {
+      _ <- database.counterTableTest.storeRecord(sample)
+      _ <-  query.flatMap(_.bind(1L, sample.id).future())
+      select2 <- database.counterTableTest.select.where(_.id eqs sample.id).one
+    } yield select2
+
+
+    whenReady(chain) { res =>
+      res.value.count shouldEqual sample.count + 1
+    }
+  }
+
   it should "allow selecting a counter" in {
     val sample = gen[CounterRecord]
 
     val chain = for {
-      incr <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 500).future()
+      _ <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 500).future()
       select <- database.counterTableTest.select.where(_.id eqs sample.id).one
-      incr <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 1).future()
+      _ <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 1).future()
       select2 <- database.counterTableTest.select(_.count_entries).where(_.id eqs sample.id).one
     } yield (select, select2)
 
@@ -66,9 +87,9 @@ class CounterColumnTest extends PhantomSuite {
     val diff = 200
 
     val chain = for {
-      incr <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 0).future()
+      _ <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 0).future()
       select <- database.counterTableTest.select.where(_.id eqs sample.id).one
-      incr <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += diff).future()
+      _ <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += diff).future()
       select2 <- database.counterTableTest.select.where(_.id eqs sample.id).one
     } yield (select, select2)
 
@@ -79,13 +100,36 @@ class CounterColumnTest extends PhantomSuite {
     }
   }
 
+
+  it should "+= counter values by a given value using prepared statements" in {
+    val sample = gen[CounterRecord]
+    val added = gen[Long]
+
+    val query = database.counterTableTest
+      .update
+      .where(_.id eqs ?)
+      .modify(_.count_entries += ?)
+      .prepareAsync()
+
+    val chain = for {
+      _ <- database.counterTableTest.storeRecord(sample)
+      _ <-  query.flatMap(_.bind(added, sample.id).future())
+      select2 <- database.counterTableTest.select.where(_.id eqs sample.id).one
+    } yield select2
+
+
+    whenReady(chain) { res =>
+      res.value.count shouldEqual sample.count + added
+    }
+  }
+
   it should "-= counter values by 1" in {
     val sample = gen[CounterRecord]
 
     val chain = for {
-      incr1 <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 1).future()
+      _ <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += 1).future()
       select <- database.counterTableTest.select.where(_.id eqs sample.id).one()
-      incr <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries -= 1).future()
+      _ <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries -= 1).future()
       select2 <- database.counterTableTest.select.where(_.id eqs sample.id).one()
     } yield (select, select2)
 
@@ -96,20 +140,71 @@ class CounterColumnTest extends PhantomSuite {
     }
   }
 
+  it should "-= counter values by 1 using prepared statements" in {
+    val sample = gen[CounterRecord]
+
+    val query = database.counterTableTest
+      .update
+      .where(_.id eqs ?)
+      .modify(_.count_entries -= ?)
+      .prepareAsync()
+
+    val chain = for {
+      _ <- database.counterTableTest.storeRecord(sample)
+      _ <-  query.flatMap(_.bind(1L, sample.id).future())
+      select2 <- database.counterTableTest.select.where(_.id eqs sample.id).one
+    } yield select2
+
+
+    whenReady(chain) { res =>
+      res.value.count shouldEqual sample.count - 1
+    }
+  }
+
   it should "-= counter values by a given value" in {
     val sample = gen[CounterRecord]
     val diff = 200
     val initial = 500
 
     val chain = for {
-      incr <-  database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries += initial).future()
-      incr <- database.counterTableTest.update.where(_.id eqs sample.id).modify(_.count_entries -= diff).future()
+      _ <-  database.counterTableTest.update
+        .where(_.id eqs sample.id)
+        .modify(_.count_entries += initial)
+        .future()
+
+      _ <- database.counterTableTest.update
+        .where(_.id eqs sample.id)
+        .modify(_.count_entries -= diff)
+        .future()
+
       select2 <- database.counterTableTest.select.where(_.id eqs sample.id).one
     } yield select2
 
 
     whenReady(chain) { res =>
       res.value.count shouldEqual (initial - diff)
+    }
+  }
+
+  it should "-= counter values by a given value using prepared statements" in {
+    val sample = gen[CounterRecord]
+    val removed = gen[Long]
+
+    val query = database.counterTableTest
+      .update
+      .where(_.id eqs ?)
+      .modify(_.count_entries -= ?)
+      .prepareAsync()
+
+    val chain = for {
+      _ <- database.counterTableTest.storeRecord(sample)
+      _ <-  query.flatMap(_.bind(removed, sample.id).future())
+      select2 <- database.counterTableTest.select.where(_.id eqs sample.id).one
+    } yield select2
+
+
+    whenReady(chain) { res =>
+      res.value.count shouldEqual sample.count - removed
     }
   }
 }
